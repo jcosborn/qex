ddc9414df5b789438e770174e1e9fed8dff2177d
James <osborn@kingly>
2016-09-26 01:29:20 -0500

    added memory footprint to benchLinalg

--- src/benchLinalg.nim
+++ src/benchLinalg.nim
@@ -1,10 +1,17 @@
+import globals
+#setForceInline(false)
+setForceInline(true)
+#setStaticUnroll(false)
+setStaticUnroll(true)
+setNoAlias(false)
+#setNoAlias(true)
+
 import qex
 import qcdTypes
 import times
 import macros
 import stdUtils
 import parseUtils
-#import optimize
 import metaUtils
 import profile
 
@@ -33,28 +40,32 @@ proc checkMem =
        getTotalMem())
   echo GC_getStatistics()
 
-template bench(fps,bps:SomeNumber; eqn:untyped) =
+template bench(fps,bps,mm,eqn: untyped) =
   let vol = lo.nSites.float
   let flops = vol * fps.float
   let bytes = vol * bps.float
-  let mm = 0
   var nrep = 1
   var dt = 0.0
   while true:
-    let t0 = getTics()
     threads:
+      threadBarrier()
+      let t0 = getTics()
       for rep in 1..nrep:
         eqn
-    let t1 = getTics()
-    dt = ticDiffSecs(t1,t0)
+      let t1 = getTics()
+      var dtt = ticDiffSecs(t1,t0)
+      threadSum(dtt)
+      threadMaster: dt = dtt/numThreads.float
     if dt>1: break
-    nrep = 1 + int(1.1*nrep.float/dt)
+    nrep = 1 + int(1.1*nrep.float/(dt+1e-9))
   let mf = int((nrep.float*flops)/(1e6*dt))
   let mb = int((nrep.float*bytes)/(1e6*dt))
   let mem = vol * (mm.float/(1024.0))
   inc nbench
   echo "bench: ",nbench| -6, "secs: ", dt|(6,3), "  mf: ", mf|7, "  mb: ", mb|7, "  mem: ", mem, "  nrep: ", nrep
   echo exp2string(eqn), "\n"
+template bench(fps,bps,eqn: untyped) =
+  bench(fps,bps,0,eqn)
 
 proc test(lat:any) =
   var lo = newLayout(lat)
@@ -67,6 +78,10 @@ proc test(lat:any) =
   var m3 = lo.ColorMatrix()
   const nc = v1[0].len
   let sf = sizeof(numberType(v3[0]))
+  let nc2 = 2*nc
+  let vb = nc2*sf
+  let mb = nc*vb
+  let mvf = (2*nc2-1)*nc2
   threads:
     m1 := 2
     v1 := 1
@@ -75,55 +90,55 @@ proc test(lat:any) =
   echo "done setup"
   var nbench = 0
 
-  bench(2*2*nc, sf*2*3*nc):
+  bench(2*nc2, 3*vb, 2*vb):
     v2 := 0.5*v2 + v1
 
-  bench((8*nc-2)*nc, sf*2*(nc+2)*nc):
+  bench(mvf, mb+2*vb, mb+2*vb):
     v2 := m1 * v1
 
-  bench((8*nc-2)*nc, sf*2*(nc+2)*nc):
+  bench(mvf, mb+2*vb, mb+2*vb):
     for e in v2:
       mulVMV(v2[e], m1[e], v1[e])
 
-  bench((8*nc)*nc, sf*2*(nc+3)*nc):
+  bench(mvf, mb+2*vb, mb+2*vb):
+    v2 := m1.adj * v1
+
+  bench(mvf+nc2, mb+3*vb, mb+2*vb):
     v2 += m1 * v1
 
-  bench((8*nc)*nc, sf*2*(nc+3)*nc):
-    for e in v2:
-      v2[e] += m1[e] * row(m2[e],0)
-
-  bench((8*nc)*nc, sf*2*(nc+3)*nc):
+  bench(mvf+nc2, mb+3*vb, mb+2*vb):
     for e in v2:
       imaddVMV(v2[e], m1[e], v1[e])
 
-  bench((8*nc-2)*nc, sf*2*(nc+2)*nc):
-    v2 := m1.adj * v1
+  bench(mvf+nc2, mb+3*vb, mb+2*vb):
+    for e in v2:
+      v2[e] += m1[e] * row(m2[e],0)
 
-  bench((8*nc-2)*nc*nc, sf*2*3*nc*nc):
+  bench(nc*mvf, 3*mb, 3*mb):
     m3 := m1 * m2
 
-  bench((8*nc-2)*nc*nc, sf*2*3*nc*nc):
+  bench(nc*mvf, 3*mb, 3*mb):
     m3 := m1.adj * m2
 
-  bench(8*nc*nc*nc, sf*2*4*nc*nc):
-    m3 += m1 * m2
-
-  bench(8*nc*nc*nc, sf*2*4*nc*nc):
-    for e in v2:
-      imaddMMM(m3[e], m1[e], m2[e])
-
-  bench((8*nc-2)*nc*nc, sf*2*3*nc*nc):
+  bench(nc*mvf, 3*mb, 3*mb):
     for e in v2:
       mulMMM(m3[e], m1[e].adj, m2[e])
 
-  bench((8*nc-2)*nc*nc, sf*2*3*nc*nc):
+  bench(nc*mvf, 3*mb, 3*mb):
     for e in m3:
       var vt{.noInit.}:type(v2[e])
       forStatic i, 0, <nc:
         mul(vt, m1[e], row(m2[e],i))
         setRow(m3[e], vt, i)
 
-  bench(8*nc*nc*nc, sf*2*3*nc*nc):
+  bench(nc*(mvf+nc2), 4*mb, 3*mb):
+    m3 += m1 * m2
+
+  bench(nc*(mvf+nc2), 4*mb, 3*mb):
+    for e in v2:
+      imaddMMM(m3[e], m1[e], m2[e])
+
+  bench(nc*(mvf+nc2), 3*mb, 2*mb):
     for e in v2:
       imaddMMM(m3[e], m1[0], m2[e])
 
@@ -131,10 +146,22 @@ proc test(lat:any) =
 qexInit()
 #checkMem()
 test([4,4,4,4])
+#test([4,4,4,8])
+#test([4,4,4,16])
+#test([8,8,8,4])
 test([8,8,8,8])
+#test([8,8,8,16])
+#test([12,12,12,8])
 test([12,12,12,12])
+#test([12,12,12,24])
+#test([16,16,16,8])
 test([16,16,16,16])
+#test([16,16,16,32])
+#test([24,24,24,12])
 test([24,24,24,24])
+#test([24,24,24,48])
+#test([32,32,32,16])
 test([32,32,32,32])
+#test([32,32,32,64])
 #checkMem()
 qexFinalize()
