d0cc7c4f8ba1d652fd3ea821b2d015d1f731f747
James <osborn@lego>
2017-05-11 14:49:00 -0500

    added missing files

---
+++ src/gauge/fat7l.nim
@@ -0,0 +1,194 @@
+import base
+import layout
+import gauge
+
+type PerfInfo* = object
+  flops*: float
+
+type Fat7lCoefs* = object
+  oneLink*: float
+  threeStaple*: float
+  fiveStaple*: float
+  sevenStaple*: float
+  lepage*: float
+
+proc computeGenStaple(staple: any, mu,nu: int, link: any, coef: float,
+                      gauge: any, fl: any, ts0: any, ts1: any,
+                      tmat1: any, tmat2: any, ts2: any) =
+  ## Computes the staple:
+  ##               mu
+  ##            +-------+
+  ##        nu  |       |
+  ##            |       |
+  ##            X       X
+  ##  Where the mu link can be any su3_matrix. The result is saved in staple.
+  ##  if staple==NULL then the result is not saved.
+  ##  It also adds the computed staple to the fatlink[mu] with weight coef.
+  mixin adj
+  # Upper staple
+  if link!=gauge[mu]:
+    #QDP_M_eq_sM(ts0, link, QDP_neighbor[nu], QDP_forward, QDP_all);
+    discard ts0 ^* link
+
+  when type(staple) isnot bool: # Save the staple
+    #QDP_M_eq_M_times_Ma(tmat1, ts0, ts1, QDP_all);
+    #QDP_M_eq_M_times_M(staple, gauge[nu], tmat1, QDP_all);
+    staple := gauge[nu] * ts0.field * ts1.field.adj
+  else:  # No need to save the staple. Add it to the fatlinks
+    #QDP_M_eq_M_times_Ma(tmat1, ts0, ts1, QDP_all);
+    #QDP_M_eq_M_times_M(tmat2, gauge[nu], tmat1, QDP_all);
+    #QDP_M_peq_r_times_M(fl[mu], &coef, tmat2, QDP_all);
+    fl[mu] += coef * gauge[nu] * ts0.field * ts1.field.adj
+
+  # lower staple
+  #QDP_M_eq_Ma_times_M(tmat1, gauge[nu], link, QDP_all);
+  #QDP_M_eq_M_times_M(tmat2, tmat1, ts1, QDP_all);
+  #QDP_M_eq_sM(ts2, tmat2, QDP_neighbor[nu], QDP_backward, QDP_all);
+  tmat2 := gauge[nu].adj * link * ts1.field
+  discard ts2 ^* tmat2
+
+  when type(staple) isnot bool: # Save the staple
+    #QDP_M_peq_M(staple, ts2, QDP_all);
+    #QDP_M_peq_r_times_M(fl[mu], &coef, staple, QDP_all);
+    staple += ts2.field
+    fl[mu] += coef * staple
+  else:  # No need to save the staple. Add it to the fatlinks
+    #QDP_M_peq_r_times_M(fl[mu], &coef, ts2, QDP_all);
+    fl[mu] += coef * ts2.field
+
+  #if(link!=gauge[mu]) QDP_discard_M(ts0);
+  #QDP_discard_M(ts2);
+
+proc makeImpLinks*(info: var PerfInfo, fl: any, gf: any, coef: any,
+                   ll: any, gfLong: any, naik: any) =
+  tic()
+  type lcm = type(gf[0])
+  proc QDP_create_M(): lcm = result.new(gf[0].l)
+  var
+    staple: lcm
+    tempmat1: lcm
+    t1: lcm
+    t2: lcm
+    tsl: array[4,Shifter[lcm,type(gf[0][0])]]
+    tsg: array[4,array[4,Shifter[lcm,type(gf[0][0])]]]
+    ts1: array[4,Shifter[lcm,type(gf[0][0])]]
+    ts2: array[4,Shifter[lcm,type(gf[0][0])]]
+    nflop = 0.0
+    dtime = 0.0
+    coef1 = coef.oneLink
+    coef3 = coef.threeStaple
+    coef5 = coef.fiveStaple
+    coef7 = coef.sevenStaple
+    coefL = coef.lepage
+    have5 = (coef5!=0.0) or (coef7!=0.0) or (coefL!=0.0)
+    have3 = (coef3!=0.0) or have5
+
+  # to fix up the Lepage term, included by a trick below
+  coef1 -= 6.0*coefL
+
+  if have3 or naik!=0.0:
+    nflop = 61632
+    staple = QDP_create_M()
+    tempmat1 = QDP_create_M()
+    if have3:
+      t1 = QDP_create_M()
+      t2 = QDP_create_M()
+      for dir in 0..<4:
+        tsl[dir] = newShifter(gf[dir], dir, 1)
+        ts1[dir] = newShifter(gf[dir], dir, 1)
+        ts2[dir] = newShifter(gf[dir], dir, 1)
+        for nu in 0..<4:
+          if dir!=nu:
+            tsg[dir][nu] = newShifter(gf[dir], nu, 1)
+            discard tsg[dir][nu] ^* gf[dir]
+
+  for dir in 0..<4:
+    #QDP_M_eq_r_times_M(fl[dir], &coef1, gf[dir], QDP_all);
+    fl[dir] := coef1 * gf[dir]
+    if have3:
+      for nu in 0..<4:
+        if nu!=dir:
+          compute_gen_staple(staple, dir, nu, gf[dir], coef3, gf, fl,
+                             tsg[dir][nu], tsg[nu][dir], t1, t2, ts2[nu])
+          if coefL!=0.0:
+            compute_gen_staple(false, dir, nu, staple, coefL, gf, fl,
+                               tsl[nu], tsg[nu][dir], t1, t2, ts2[nu])
+          if coef5!=0.0 or coef7!=0.0:
+            for rho in 0..<4:
+              if (rho!=dir) and (rho!=nu):
+                compute_gen_staple(tempmat1, dir, rho, staple, coef5, gf, fl,
+                                   tsl[rho], tsg[rho][dir], t1, t2, ts2[rho])
+                if coef7!=0.0:
+                  for sig in 0..<4:
+                    if (sig!=dir) and (sig!=nu) and (sig!=rho):
+                      compute_gen_staple(false, dir, sig, tempmat1,coef7,gf,fl,
+                                         ts1[sig],tsg[sig][dir],t1,t2,ts2[sig])
+
+  # long links
+  if naik!=0.0:
+    for dir in 0..<4:
+      #QDP_M_eq_sM(staple, gfLong[dir], QDP_neighbor[dir], QDP_forward,QDP_all)
+      #QDP_M_eq_M_times_M(tempmat1, gfLong[dir], staple, QDP_all)
+      #QDP_M_eq_sM(staple, tempmat1, QDP_neighbor[dir], QDP_forward, QDP_all)
+      #QDP_M_eq_M_times_M(ll[dir], gfLong[dir], staple, QDP_all)
+      #QDP_M_eq_r_times_M(ll[dir], &naik, ll[dir], QDP_all)
+      discard tsl[dir] ^* gfLong[dir]
+      discard ts1[dir] ^* (gfLong[dir] * tsl[dir].field)
+      discard ts2[dir] ^* (gfLong[dir] * ts1[dir].field)
+      ll[dir] := naik * ts2[dir].field
+
+  #[
+  if have3 or naik!=0.0:
+    QDP_destroy_M(staple);
+    QDP_destroy_M(tempmat1);
+    if have3:
+      QDP_destroy_M(t1);
+      QDP_destroy_M(t2);
+      for dir in 0..<4:
+        QDP_destroy_M(tsl[dir]);
+        QDP_destroy_M(ts1[dir]);
+        QDP_destroy_M(ts2[dir]);
+        for nu in 0..<4:
+          if(tsg[dir][nu]!=nil) QDP_destroy_M(tsg[dir][nu])
+  ]#
+  #info.final_sec = dtime;
+  #info.final_flop = nflop*QDP_sites_on_node;
+  #info.status = QOP_SUCCESS;
+  toc()
+proc makeImpLinks*(info: var PerfInfo, fl: any, gf: any, coef: any) =
+  makeImpLinks(info, fl, gf, coef, fl, gf, 0.0)
+
+when isMainModule:
+  import qex
+  import physics/qcdTypes
+  import gauge
+  qexInit()
+  #var defaultGaugeFile = "l88.scidac"
+  let defaultLat = @[8,8,8,8]
+  defaultSetup()
+  #for mu in 0..<g.len: g[mu] := 1
+  #g.random
+
+  var info: PerfInfo
+  var coef: Fat7lCoefs
+  coef.oneLink = 1.0
+  coef.threeStaple = 1.0
+  coef.fiveStaple = 1.0
+  coef.sevenStaple = 1.0
+  coef.lepage = 1.0
+  var naik = 1.0
+
+  var fl = lo.newGauge()
+  var ll = lo.newGauge()
+  var g3 = g
+
+  echo g.plaq
+  makeImpLinks(info, fl, g, coef)
+  makeImpLinks(info, fl, g, coef, ll, g3, naik)
+  echo fl.plaq
+  echo ll.plaq
+  echo pow(1.0,4)/6.0
+  echo pow(1.0+6.0,4)/6.0
+  echo pow(1.0+6.0+6.0*4.0,4)/6.0
+  echo pow(1.0+6.0+6.0*4.0+6.0*4.0*2.0,4)/6.0
+  echo pow(1.0+6.0+6.0*4.0+6.0*4.0*2.0+6.0,4)/6.0
---
+++ src/physics/stagMesonLocal.nim
@@ -0,0 +1,139 @@
+import base
+import gauge
+import os
+
+template corner(l, i):untyped =
+  (l.coords[0][i].int and 1) + ((l.coords[1][i].int and 1) shl 1) +
+   ((l.coords[2][i].int and 1) shl 2)
+
+template addCorner(l, s, i):untyped =
+  ((s + l.coords[0][i].int) and 1) +
+   ((((s shr 1) + l.coords[1][i].int) and 1) shl 1) +
+   ((((s shr 2) + l.coords[2][i].int) and 1) shl 2)
+
+proc stagMesons*(v: any) =
+  let l = v.l
+  let nt = l.physGeom[3]
+  var c = newSeq[array[8,float]](nt)
+  when true:
+  #when false:
+    #var x:VectorArray[3,DComplex]
+    for i in l.sites:
+      let t = l.coords[3][i]
+      let s = l.corner(i)
+      c[t][s] += v{i}.norm2()
+      #assign(x, v{i})
+      #c[t][s] += x.norm2
+  else:
+    threads:
+      #var x:VectorArray[3,DComplex]
+      for i in 0..<l.nSites:
+        let t = l.coords[3][i]
+        let s = l.corner(i)
+        let tpar = (8*t+s) mod numThreads
+        if tpar==threadNum:
+          c[t][s] += v{i}.norm2()
+          #assign(x, v{i})
+          #c[t][s] += x.norm2
+  rankSum(c)
+  for s in 0..<8:
+    echo "corner: ", s
+    for t in 0..<nt:
+      let r = c[t][s]
+      echo t, " ", r
+      #echo t, " ", c[t][s]
+  echo "sum:"
+  for t in 0..<nt:
+    var r = c[t][0]
+    for s in 1..<8:
+      r += c[t][s]
+    echo t, " ", r
+
+proc stagMesonsV*(v: any) =
+  let l = v.l
+  let nt = l.physGeom[3]
+  var cv = newAlignedMem[array[8,type(v[0].norm2())]](nt)
+  var c = newSeq[array[8,float64]](nt)
+  threads:
+    for e in 0..<l.nSitesOuter:
+      let i = l.nSitesInner * e
+      let t = l.coords[3][i]
+      let s = l.corner(i)
+      let tpar = (8*t+s) mod numThreads
+      if tpar==threadNum:
+        cv[t][s] += v[e].norm2
+  for tt in 0..<nt:
+    let tt0 = tt - l.coords[3][0] + nt
+    for ss in 0..<8:
+      for i in 0..<l.nSitesInner:
+        let t = (tt0 + l.coords[3][i]) mod nt
+        let s = l.addCorner(ss, i)
+        c[t][s] += cv[tt][ss][i]
+  rankSum(c)
+  for s in 0..<8:
+    echo "corner: ", s
+    for t in 0..<nt:
+      let r = c[t][s]
+      echo t, " ", r
+      #echo t, " ", c[t][s]
+  echo "sum:"
+  for t in 0..<nt:
+    var r = c[t][0]
+    for s in 1..<8:
+      r += c[t][s]
+    echo t, " ", r
+
+when isMainModule:
+  import qex
+  import physics/qcdTypes
+  import physics/stagD
+  qexInit()
+  var defaultGaugeFile = "l88.scidac"
+  #var defaultLat = [4,4,4,4]
+  var defaultLat = [8,8,8,8]
+  #var defaultLat = @[8,8,8,16]
+  #var defaultLat = @[12,12,12,12]
+  defaultSetup()
+  var v1 = lo.ColorVector()
+  var v2 = lo.ColorVector()
+  var r = lo.ColorVector()
+  threads:
+    g.setBC
+    g.stagPhase
+    v1 := 0
+    #for e in v1:
+    #  template x(d:int):untyped = lo.vcoords(d,e)
+    #  v1[e][0].re := foldl(x, 4, a*10+b)
+    #  #echo v1[e][0]
+  #echo v1.norm2
+  if myRank==0:
+    v1{0}[0] := 1
+    #v1{2*1024}[0] := 1
+  echo v1.norm2
+  var s = newStag(g)
+  var m = 0.001
+  threads:
+    v2 := 0
+    echo v2.norm2
+    threadBarrier()
+    s.D(v2, v1, m)
+    threadBarrier()
+    #echoAll v2
+    echo v2.norm2
+  #echo v2
+  s.solve(v2, v1, m, 1e-8)
+  resetTimers()
+  s.solve(v2, v1, m, 1e-8)
+  threads:
+    echo "v2: ", v2.norm2
+    echo "v2.even: ", v2.even.norm2
+    echo "v2.odd: ", v2.odd.norm2
+    s.D(r, v2, m)
+    threadBarrier()
+    r := v1 - r
+    threadBarrier()
+    echo r.norm2
+  #echo v2
+  stagMesons(v2)
+  stagMesonsV(v2)
+  qexFinalize()
