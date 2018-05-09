43edf9c839c3bc5a2f7799a3e82f9841f0619a87
James Osborn <osborn@alcf.anl.gov>
2018-05-08 20:51:05 -0500

    small change

--- src/rng/distribution.nim
+++ src/rng/distribution.nim
@@ -152,7 +152,7 @@ proc u1*(x: var AsComplex, r: var RNG) =
       x.re *= s
       x.im *= s
   else:
-    let n = (x.numberType)(2.0 * PI * r.uniform)
+    let n = 2.0 * PI * r.uniform.float
     x.re = cos n
     x.im = sin n
 proc u1*(x: var AsVector, r: var RNG) =
