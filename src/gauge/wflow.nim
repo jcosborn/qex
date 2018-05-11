1d85694ca7b9fcd67b5dfa7877a2ddc4e2548d11
Xiao-Yong Jin <xjin@anl.gov>
2018-05-11 12:00:56 -0500

    fix wflow

--- src/gauge/wflow.nim
+++ src/gauge/wflow.nim
@@ -21,52 +21,43 @@ Zi = eps Z(Wi)
 template gaugeFlow*(g: array|seq, steps: int, eps: float, measure: untyped): untyped =
   ## Wilson flow.
   ## The input gauge field will be modified.
-  ## `wflowT` ang `wflowG` is injected for `measure`.
-  var
-    p = g[0].l.newGauge  # mom
-    f = g[0].l.newGauge  # force
-  # threads:
-  block:
-    for n in 1..steps:
-      let t = n * eps
-      #threadBarrier()
-      f.gaugeForce2 g
-      #threadBarrier()
-      for mu in 0..<f.len:
-        #echo t, " f[", mu, "]: ", f[mu].norm2
-        p[mu] := (-1.0/4.0)*eps*f[mu]
-        for e in g[mu]:
-          let t = exp(p[mu][e])*g[mu][e]
-          g[mu][e] := t
-      #threadBarrier()
-      #block:
-      #  let
-      #    p = g.plaq
-      #    ps = 2.0*(p[0]+p[1]+p[2])
-      #    pt = 2.0*(p[3]+p[4]+p[5])
-      #  echo "plaq ss: ",ps," st: ",pt," tot: ",p.sum
-      f.gaugeForce2 g
-      #threadBarrier()
-      for mu in 0..<f.len:
-        #echo t, " f[", mu, "]: ", f[mu].norm2
-        p[mu] := (-8.0/9.0)*eps*f[mu] + (-17.0/9.0)*p[mu]
-        for e in g[mu]:
-          let t = exp(p[mu][e])*g[mu][e]
-          g[mu][e] := t
-      #threadBarrier()
-      f.gaugeForce2 g
-      #threadBarrier()
-      for mu in 0..<f.len:
-        #echo t, " f[", mu, "]: ", f[mu].norm2
-        p[mu] := (-3.0/4.0)*eps*f[mu] - p[mu]
-        for e in g[mu]:
-          let t = exp(p[mu][e])*g[mu][e]
-          g[mu][e] := t
-      block:
-        let
-          wflowT {.inject.} = t
-          wflowG {.inject.} = g
-        measure
+  ## `wflowT` and `wflowG` is injected for `measure`.
+  proc flowProc {.gensym.} =
+    const nc = g[0][0].nrows.float
+    var
+      p = g[0].l.newGauge  # mom
+      f = g[0].l.newGauge  # force
+    block:
+      for n in 1..steps:
+        let t = n * eps
+        let epsnc = eps * nc  # compensate force normalization
+        f.gaugeForce2 g
+        threads:
+          for mu in 0..<f.len:
+            p[mu] := (-1.0/4.0)*epsnc*f[mu]
+            for e in g[mu]:
+              let t = exp(p[mu][e])*g[mu][e]
+              g[mu][e] := t
+        f.gaugeForce2 g
+        threads:
+          for mu in 0..<f.len:
+            p[mu] := (-8.0/9.0)*epsnc*f[mu] + (-17.0/9.0)*p[mu]
+            for e in g[mu]:
+              let t = exp(p[mu][e])*g[mu][e]
+              g[mu][e] := t
+        f.gaugeForce2 g
+        threads:
+          for mu in 0..<f.len:
+            p[mu] := (-3.0/4.0)*epsnc*f[mu] - p[mu]
+            for e in g[mu]:
+              let t = exp(p[mu][e])*g[mu][e]
+              g[mu][e] := t
+        block:
+          let
+            wflowT {.inject.} = t
+            wflowG {.inject.} = g
+          measure
+  flowProc()
 
 when isMainModule:
   import qex, gauge, physics/qcdTypes
@@ -99,21 +90,32 @@ when isMainModule:
     echo "WFLOW ",wflowT
     wflowG.printPlaq
 
-  if fn.len == 0:
-    const
-      p0 = @[
-        0.01960725848281519,
-        0.01982378149813489,
-        0.01938877647467847,
-        0.0185899778070918,
-        0.0180821938831715,
-        0.01876842496122964]
-      p0s = p0.sum
-    let p = g.plaq
-    if abs(p.sum-p0s)/p0s > 1e-14:
-      echo "Test failed."
-      echo "Expected:\t",p0
-      echo "Actual:\t",p
-      qexExit -1
+  when g[0][0].nrows == 1 or g[0][0].nrows == 3:
+    if fn.len == 0:
+      when g[0][0].nrows == 1:
+        const
+          p0 = @[
+            0.02071819961326493,
+            0.01982444094659434,
+            0.01998727952235725,
+            0.01837579824880835,
+            0.01528029895697336,
+            0.01782410235480616]
+      when g[0][0].nrows == 3:
+        const
+          p0 = @[
+            0.01960725848281519,
+            0.01982378149813489,
+            0.01938877647467847,
+            0.0185899778070918,
+            0.0180821938831715,
+            0.01876842496122964]
+      const p0s = p0.sum
+      let p = g.plaq
+      if abs(p.sum-p0s)/p0s > 1e-14:
+        echo "Test failed."
+        echo "Expected:\t",p0
+        echo "Actual:\t",p
+        qexExit -1
 
   qexFinalize()
