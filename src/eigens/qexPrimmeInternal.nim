dc32a8a65b74b8befce9dc3b5dfa1c7c2b3e4bbd
Xiao-Yong Jin <xjin@anl.gov>
2018-12-12 16:29:22 -0600

    qexPrimmeInternal: add conversions for gauge arrays

--- src/eigens/qexPrimmeInternal.nim
+++ src/eigens/qexPrimmeInternal.nim
@@ -41,4 +41,33 @@ proc fromPrimmeArray*(f:Field, a:ptr ccomplex[float], skip:int = 0) =
     f[cl*s+j] = a[cl*i+2*j]
     f[cl*s+j+vl] = a[cl*i+2*j+1]
 
+# WARNING: low level implementation details follow.
+template convPrimmeArrayGauge(ff:seq[Field], aa:ptr PRIMME_COMPLEX_DOUBLE, ss:int, body:untyped) {.dirty.} =
+  const
+    nc = ff[0][0].nrows
+    vl = ff[0].V
+    cl = 2*vl
+  let
+    nd = ff.len
+    n = nc*nc*f[0].l.nSites div vl
+    skip = nc*nc*ss div vl
+  for mu in 0..<nd:
+    var
+      a = asarray[float](asarray[float](aa)[mu*cl*n].addr)
+      f = asarray[float]ff[mu].s.data
+    tfor i, 0..<n:
+      let s = i + skip
+      forO j, 0, vl.pred:
+        body
+proc toPrimmeArrayGauge*(f:seq[Field], a:ptr PRIMME_COMPLEX_DOUBLE, skip:int = 0) =
+  ## `skip` is in units of sites.
+  convPrimmeArrayGauge(f,a,skip):
+    a[cl*i+2*j] = f[cl*s+j]
+    a[cl*i+2*j+1] = f[cl*s+j+vl]
+proc fromPrimmeArrayGauge*(f:seq[Field], a:ptr PRIMME_COMPLEX_DOUBLE, skip:int = 0) =
+  ## `skip` is in units of sites.
+  convPrimmeArrayGauge(f,a,skip):
+    f[cl*s+j] = a[cl*i+2*j]
+    f[cl*s+j+vl] = a[cl*i+2*j+1]
+
 template ff*(x:untyped):auto = formatFloat(x,ffScientific,17)
