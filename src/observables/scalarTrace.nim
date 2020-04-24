57b88caa0cc9e9633ffd997492192e3b9461df10
Xiao-Yong Jin <xjin@anl.gov>
2020-04-24 12:01:20 -0700

    scalarTrace: GC smearing; use letParam; more qexLog; echoTimers

--- src/observables/scalarTrace.nim
+++ src/observables/scalarTrace.nim
@@ -19,46 +19,50 @@
 ]#
 
 import qex, gauge/hypsmear, physics/stagSolve
-import times
+import times, os
 
 qexInit()
 
 # Accept command line arguments and set up parameters
-let
+letParam:
   # Required parameters
-  inlat = strParam("inlat")   # Input gauge file name
-  outfn = strParam("outfn", "output")   # Save results to outfn.{trace,noise,prop}
-  mass = floatParam("mass", 0.1)   # The quark mass
+  inlat = ""   # Input gauge file name
+  outfn = "output" # Save results to outfn
+  mass = 0.1   # The quark mass
+
+  # Intermediate definitions
+  lat =
+    if existsFile(inlat):
+      getFileLattice inlat
+    else:
+      qexWarn "Nonexistent gauge file: ", inlat
+      @[4,4,4,8]
 
   # optional parameters
-  cg_prec = floatParam("cg_prec", 1e-9) # Max residual with default
-  cg_max = intParam("cg_max", 100_000)  # Max number of iterations
-  num_stoch = intParam("num_stoch", 1)  # Number of stochastic sources
-  improved_trace = intParam("improved_trace", 1).bool
-  save_props = intParam("save_props", 0).bool
-  source_type = strParam("source_type", "Z4") # Z4, Z2, U1, Gauss
-  dilute_type = strParam("dilute_type", "EO").parseDilution # EO, CORNER
-  seed = intParam("seed", int(1000*epochTime())).uint64
-  # write_group_size = intParam("write_group_size", 128)
+  sloppy:SloppyType = 2 # 0: None, 1: Single, 2: Half
+  cg_prec = 1e-9 # Max residual with default
+  cg_max = 100_000  # Max number of iterations
+  num_stoch = 1  # Number of stochastic sources
+  improved_trace:bool = 1
+  save_props:bool = 0
+  source_type = "Z4" # Z4, Z2, U1, Gauss
+  dilute_type:parseDilution = "EO" # EO, CORNER
+  seed:uint64 = int(1000*epochTime())
+  ## write_group_size = 128
+  showTimers:bool = 0 # print out the timers in the end
+
+echoParams()
+
+echo "rank ", myRank, "/", nRanks
+threads: echo "thread ", threadNum, "/", numThreads
 
 var sp = initSolverParams()
 sp.r2req = cg_prec*cg_prec
 sp.maxits = cg_max
-sp.sloppySolve = intParam("sloppy", 2).SloppyType # 0: None, 1: Single, 2: Half
+sp.sloppySolve = sloppy
 
 # Load lattice and determine size and parameters
-template getLat(fn:string): seq[int] =
-  if inlat.len == 0:
-    echo "WARNING: no input lattice file specicified."
-    @[8,8,8,8]
-  else:
-    let lat = inlat.getFileLattice
-    if lat.len == 0:
-      echo "ERROR: getFileLattice failed on '", inlat, "'."
-      qexAbort()
-    lat
 let
-  lat = inlat.getLat
   nt = lat[^1]
   metadata_prefix = "l" & $lat[0] & ".t" & $nt & ".m" & $mass & ".cfg" & inlat
 
@@ -82,12 +86,8 @@ else:
 
 echo "latsize = ",lo.physGeom
 echo "volume = ",lo.physVol
-echo "mass = ",mass
-echo "seed = ",seed
-echo "cg_prec = ",cg_prec
-echo "num_stoch = ",num_stoch
-echo "source_type = ",source_type
-echo "dilute_type = ",dilute_type
+
+qexLog "Finished loading conifguration."
 
 var spatv = 1
 for i in 0..<lat.len-1: spatv *= lat[i]
@@ -112,17 +112,30 @@ threads:
   echo "new unitary deviation avg: ",d.avg," max: ",d.max
 g.printPlaq
 
-var
-  info: PerfInfo
-  coef = HypCoefs(alpha1:0.4, alpha2:0.5, alpha3:0.5)
-echo "smear = ",coef
+qexLog "Finished re-unitarization."
+
+proc smear(sg,g:any) =
+  var
+    info: PerfInfo
+    coef = HypCoefs(alpha1:0.4, alpha2:0.5, alpha3:0.5)
+  echo "smear = ",coef
+  coef.smear(g, sg, info)
+
 var sg = lo.newGauge
-coef.smear(g, sg, info)
+sg.smear g
+
+#echo GC_getStatistics()
+GC_fullCollect()
+#echo GC_getStatistics()
+
+qexLog "Finished smearing."
 
 threads:
   sg.setBC
   sg.stagPhase
 var s = sg.newStag
+
+qexLog "Finished stagPhase."
 
 var
   eta = lo.ColorVector        # Noise source
@@ -131,7 +144,7 @@ var
   tmps = lo.ColorVector
 
 for i in 0..<num_stoch:
-  echo "Starting work on noise source ",i," ."
+  qexLog "Starting work on noise source ",i," ."
   phi := 0
   trce := 0
 
@@ -146,17 +159,16 @@ for i in 0..<num_stoch:
       threadBarrier()
       eta *= 1.0/sqrt(2.0)
     else:
-      echo "ERROR: Invalid noise type ",source_type,"."
-      qexAbort()
+      qexError "Invalid noise type ",source_type,"."
     threadBarrier()
     echo "noise norm2: ",eta.norm2
 
   if save_props:              # XXX save eta
-    echo "WARNING: save not implemented"
+    qexWarn "Save not implemented"
 
   for t in 0..<nt:
     for dl in dilution(dilute_type):
-      echo "Source ",i," dilution pattern ",dl," timeslice ",t," ."
+      qexLog "Source ",i," dilution pattern ",dl," timeslice ",t," ."
       threads:
         tmps := 0
         threadBarrier()
@@ -174,6 +186,7 @@ for i in 0..<num_stoch:
       s.solve(phi, tmps, mass, sp)
       threads:
         echo "dest norm2: ",phi.norm2
+      qexLog "Finished one solve."
 
       # if save_props:        # XXX save phi
       #   echo "WARNING: save not implemented"
@@ -187,6 +200,8 @@ for i in 0..<num_stoch:
           echo "Computing the unimproved trace."
           for i in trce:
             trce[i] += tmps[i].dot phi[i]
+      qexLog "Finished one trace."
+  qexLog "Finished source ",i," ."
 
   threads:
     let invnc = 1.0 / tmps[0].len.float
@@ -200,14 +215,16 @@ for i in 0..<num_stoch:
   est.ranksum
   for t in 0..<nt:
     echo "initsrc ",i," timeslice ",t," pbp ",est[t] / spatv.float
+  qexLog "Finished computing source ",i," ."
 
   var trceWriter = lo.newWriter(trace_file(i), trace_meta(i))
   trceWriter.write(trce, trace_meta(i), "D")
   trceWriter.close
+  qexLog "Finished saving trace."
 
 # Test loading traces
 for i in 0..<num_stoch:
-  echo "Loading trace from source ", i
+  qexLog "Loading trace from source ", i
 
   var trceReader = lo.newReader trace_file(i)
   trceReader.read trce
@@ -228,4 +245,5 @@ for i in 0..<num_stoch:
 
   # per-slice estimate at +z momentum XXX
 
+if showTimers: echoTimers()
 qexFinalize()
