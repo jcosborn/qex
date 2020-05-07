9ce4d83a128085bd8910ab161d93f49f10a046e0
James Osborn <osborn@Jamess-MacBook-Pro.local>
2020-05-07 10:18:55 -0500

    disable MPI CI for now

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 export OMP_NUM_THREADS=2
-export RUNJOB="mpirun -np 2 --bind-to none"
+export RUNJOB="mpirun -np 2"
+#export RUNJOB="mpirun -np 2 --bind-to none"
 #export RUNJOB="true"
 
 ./testscript.sh
