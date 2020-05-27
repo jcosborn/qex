4868324fe37f757a2fde239c6c8a1eca3d3d9584
Xiao-Yong Jin <xjin@anl.gov>
2020-05-27 11:19:19 -0500

    runtests with bound procs

--- runtests.sh
+++ runtests.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 export OMP_NUM_THREADS=2
-export RUNJOB="mpirun -np 2"
-#export RUNJOB="mpirun -np 2 --bind-to none"
+export RUNJOB="mpiexec -n 2 -bind-to user:0,1"
+#export RUNJOB="mpiexec -n 2 --bind-to none"
 #export RUNJOB="true"
 
 ./testscript.sh
