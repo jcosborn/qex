9e69944e7048308c76fe5b65e6a947f0a69fe42a
Xiao-Yong Jin <xjin@anl.gov>
2023-02-10 16:07:20 -0600

    algorithms/numdiff: fix ndiff, support matrix valued functions

--- src/algorithms/numdiff.nim
+++ src/algorithms/numdiff.nim
@@ -5,11 +5,26 @@ type UpTri[N:static int,T] = object
 
 func upTriIndex[N:static int](i,j:int): int =
   # 0<=i<=j<N
-  ((N+N+1-i)*i div 2) + j - i
+  # ((N+N+1-i)*i div 2) + j - i    # row major
+  ((1+j)*j div 2) + i    # col major
 
 proc `[]`[N,T](x: UpTri[N,T], i,j: int): T = x.d[upTriIndex[N](i,j)]
 proc `[]`[N,T](x: var UpTri[N,T], i,j: int): var T = x.d[upTriIndex[N](i,j)]
 proc `[]=`[N,T](x: var UpTri[N,T], i,j: int, y: T) = x.d[upTriIndex[N](i,j)] = y
+
+proc maxAbsElem(z: var auto, x: auto, y: auto) =
+  when compiles(len(z)) and compiles(z[0]):
+    # array, try element wise max and abs
+    for i in 0..<z.len:
+      maxAbsElem(z[i], x[i], y[i])
+  elif compiles(nrows(z)) and compiles(ncols(z)) and compiles(z[0,0]):
+    # a kind of matrix
+    for i in 0..<z.nrows:
+      for j in 0..<z.ncols:
+        maxAbsElem(z[i,j], x[i,j], y[i,j])
+  else:
+    # hope for the best
+    z = max(abs(x), abs(y))
 
 template ndiffTemplate(derivative: untyped) {.dirty.} =
   #[
@@ -18,47 +33,45 @@ template ndiffTemplate(derivative: untyped) {.dirty.} =
     Advances in Engineering Software (1978)
     Volume 4, Issue 2, April 1982, Pages 75-76
 
-    Note: Ridders refer the method as Romberg's, though Romberg's is for integrals.
+    Note: Ridders refers the method as Romberg's, though Romberg's is for integrals.
     It looks like an extension to Neville's algorithm.
   ]#
   let s2 = scale*scale
   var dx = dx
   var A: UpTri[ordMax, F]
   for i in 0..<ordMax:
-    A[0,i] = (f(x+dx) - f(x-dx)) * (0.5/dx)
+    A[0,i] = derivative
     # echo "A[0,",i,"] = ",A[0,i]
     dx /= scale
     var b = s2
     for j in 1..i:
-      let ai = A[j-1,i]
+      var ai = A[j-1,i]
+      ai *= b
       let ai1 = A[j-1,i-1]
-      A[j,i] = (ai*b - ai1) * (1.0/(b - 1.0))
+      let c = 1.0/(b-1.0)
+      A[j,i] = (ai - ai1) * c
       # echo "A[",j,",",i,"] = ",A[j,i]
       b *= s2
   let a = A[ordMax-1,ordMax-1]
   let a1 = A[ordMax-2,ordMax-1]
   let a2 = A[ordMax-2,ordMax-2]
-  let e1 = a-a1
-  let e2 = a-a2
-  when compiles(len(err)):
-    # array, try element wise max and abs
-    for i in 0..<err.len:
-      err[i] = max(abs(e1[i]), abs(e2[i]))
-  else:
-    err = max(abs(e1), abs(e2))
+  maxAbsElem(err, a-a1, a-a2)
   r = a
 
 proc ndiff*[X,F](r: var F, err: var F, f: proc(x:X):F, x: X, dx: auto, scale:float=2.0, ordMax:static int=8) =
   ## return r = f'(x), using polynomial approximation with points x+/-dx, with dx/=scale, upto ordMax
   ## err is the difference in the estimate of the current and the previous order.
+  ## F can be multidimensional, X is scalar
   ndiffTemplate:
-    (f(x+dx) - f(x-dx)) / (2*dx)
+    (f(x+dx) - f(x-dx)) * (0.5/dx)
 
 proc ndiff2*[X,F](r: var F, err: var F, f: proc(x:X):F, x: X, dx: auto, scale:float=2.0, ordMax:static int=8) =
   ## return r = f''(x), using polynomial approximation with points x+/-dx, with dx/=scale, upto ordMax
   ## err is the difference in the estimate of the current and the previous order.
+  ## F can be multidimensional, X is scalar
   ndiffTemplate:
-    (f(x+dx) - 2.0*f(x) + f(x-dx)) / (dx*dx)
+    let idx = 1.0/dx
+    (f(x+dx) - 2.0*f(x) + f(x-dx)) * (idx*idx)
 
 when isMainModule:
   import math
@@ -72,8 +85,8 @@ when isMainModule:
   echo "Analytic: ", a
   echo "Numeric:  ", r, "  error: ",r-a,"  estimated: ",e
 
-  echo "ordMax = 8"
-  ndiff(r, e, f, 1.0, 0.01, ordMax=8)
+  echo "dx = 0.1, scale=5.0, ordMax=5"
+  ndiff(r, e, f, 1.0, 0.1, scale=5.0, ordMax=5)
   echo "Numeric:  ", r, "  error: ",r-a,"  estimated: ",e
 
   # example 2, exp(x)
@@ -100,9 +113,16 @@ when isMainModule:
   v[2] = -1.0
   v[3] = 10.0
   proc f(x:SimdD4):SimdD4 = exp(x)/(sin(x)-x*x)
-  ndiff(vr, ve, f , v, 0.01)
+  ndiff(vr, ve, f , v, 0.05, scale=5.0, ordMax=5)
   let vex = exp(v)
   let vd = sin(v)-v*v
-  let va = vex/vd - vex*(cos(v)-2.0*v)/(vd*vd)
+  let va = vex/vd*(1.0 - (cos(v)-2.0*v)/vd)
   echo "Analytic: ", va
   echo "Numeric:  ", vr, "  error: ",vr-va,"  estimated: ",ve
+
+  ndiff2(vr, ve, f , v, 0.05, scale=3.5, ordMax=6)
+  let vc = cos(v)-2.0*v
+  let va2 = vex/vd*(1.0 - (2.0*vc - sin(v) - 2.0 - 2.0*vc*vc/vd)/vd)
+  echo "2nd derivative:"
+  echo "Analytic: ", va2
+  echo "Numeric:  ", vr, "  error: ",vr-va2,"  estimated: ",ve
