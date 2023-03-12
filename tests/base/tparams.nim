42b8995a001f1c2c58d9045aa01b917d0e09e8f6
James Osborn <osborn@alcf.anl.gov>
2023-03-11 20:05:26 -0600

    add test for params

---
+++ tests/base/tparams.nim
@@ -0,0 +1,48 @@
+import base/params
+
+proc paramTest* =
+  var i = intParam("i", 0, "Test intParam")
+  var f = floatParam("f", 0.0, "Test floatParam")
+  echo i, f
+
+when isMainModule:
+  import qex
+  qexInit()
+
+  var b = true
+  var x = 1
+
+  letParam:
+    bf = false
+    bt = true
+    bs = b
+    bx = if b: true else: false
+    i0 = 0
+    i1 = 1
+    ix = if true: 2 else: 3
+    f0 = 0.0
+    f1 = 1.0
+    fx = if true: 2.0 else: 3.0
+    s0 = "foo0"
+    s1 = "foo1"
+    sx = if true: "foo2" else: "foo3"
+    ia0 = @[0,0,0,0]
+    ia1 = @[1,1,1,1]
+    iax = if true: @[2,2,2,2] else: @[3,3,3,3]
+    fa0 = @[0.0,0,0,0]
+    fa1 = @[1.0,1,1,1]
+    fax = if true: @[2.0,2,2,2] else: @[3.0,3,3,3]
+
+  #installLoadParams()
+  #installSaveParams()
+  #installHelpParam()
+  installStandardParams()
+  echoParams()
+  processHelpParam()
+
+  defaultSetup()
+  paramTest()
+
+  processSaveParams()
+  writeParamFile()
+  qexFinalize()
