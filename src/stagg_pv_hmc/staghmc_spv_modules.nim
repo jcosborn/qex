6e9c996d9aba0461fcc1debe8cfce0b84f3b73b6
Curtis Peterson <cpeterso@lq.fnal.gov>
2023-06-05 14:07:25 -0500

    Updated "staghmc_spv_modules.nim" to include modified force calculation code

--- src/stagg_pv_hmc/staghmc_spv_modules.nim
+++ src/stagg_pv_hmc/staghmc_spv_modules.nim
@@ -2,8 +2,10 @@
 import staghmc_spv_init_and_io # Initialization and IO
 import staghmc_spv_meas # Measurements
 import staghmc_spv_rng # Manipulate global RNG
+import staghmc_spv_gforce # Modified force procs for gauge action smearing
 
 #[ Exports ]#
 export staghmc_spv_init_and_io
 export staghmc_spv_meas
 export staghmc_spv_rng
+export staghmc_spv_gforce
