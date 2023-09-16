73b5e856b9e66f7e517becd169f17f81b2d1faeb
James Osborn <osborn@alcf.anl.gov>
2023-09-16 13:03:25 -0500

    debugging CI

--- src/field/fieldET.nim
+++ src/field/fieldET.nim
@@ -377,7 +377,9 @@ iterator sites*(s:Subset):int {.inline.} =
 #  let n = x.l.nSitesOuter
 #  itemsI(0, n)
 iterator items*(x:Field):int {.inline.} =
-  let n = x.l.nSitesOuter
+  #let n = x.l.nSitesOuter
+  let l = x.l
+  let n = l.nSitesOuter
   #echo "n: ", n
   itemsI(0, n)
 iterator items*(x:Subsetted):int {.inline.} =
