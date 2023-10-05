9df8f6036c6584e8b05365c30fcef35a7d884103
James Osborn <osborn@alcf.anl.gov>
2023-10-05 10:18:16 -0500

    display which reader used at compile time

--- src/io/reader.nim
+++ src/io/reader.nim
@@ -1,8 +1,10 @@
 import os
 
 when defined(QioReader) or not defined(QioliteReader):
+  static: echo "using QIO reader"
   import readerQio
   export readerQio
 else:
+  static: echo "using Qiolite reader"
   import readerQiolite
   export readerQiolite
