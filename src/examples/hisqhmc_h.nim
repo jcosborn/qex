18cefc3c007520e6972f511a9b160c50cf79e038
ctpeterson <curtistaylorpetersonwork@gmail.com>
2025-06-02 15:02:15 -0400

    removed outdated hisq_force.nim & hisqhmc.nim. updated hisqhmc_h.nim.

--- src/examples/hisqhmc_h.nim
+++ src/examples/hisqhmc_h.nim
@@ -1,13 +1,35 @@
-# A bit of a lazy Hasenbusch "upgrade" to hisqhmc.nim
+## Brief: Four-flavor highly improved staggered quark (HISQ) HMC w/ Hasenbusch
+## 
+## Author: Curtis Taylor Peterson, James Osborn
+## 
+## Details:
+## HISQ HMC with Hasenbusch preconditioning. Note that this specific code
+## does not come with support for rooting. Additionally, the tadpole 
+## factor is set to unity, so that the gauge action is plaquette + rectangle. 
+## Due to the way in which the plaquette and rectangle coefficients are set,
+## the bare gauge coupling automatically inherits the MILC convention of
+## beta = 10/g_0^2, as opposed to the conventional beta = 6/g_0^2. 
+## 
+## This code can be used both as a standalone program and as a module that
+## is to be imported by a standalone program. See the code under "isMainModule"
+## for an example of how to deploy it for your purposes. 
+## 
+## Information is specified to program by input flags and a JSON file. See
+## "newHISQHMC" input flags and "defaultInputs" for an example of what goes 
+## into the JSON file.
+
 import qex
-import mdevolve
-import times,macros,json,parseopt,sequtils
-import strformat,strutils,streams,os
+import base
 import gauge
 import layout
 import gauge/[hisqsmear]
 import algorithms/[integrator]
 import physics/[qcdTypes,stagSolve]
+
+import mdevolve
+
+import times,macros,json,parseopt,sequtils
+import strformat,strutils,streams,os
 
 export integrator
 export mdevolve
@@ -39,6 +61,7 @@ const
   ForceMaxCGIter* = 10000
   ActionCGVerbosity* = 1
   ForceCGVerbosity* = 1
+  SmearingVerbosity* = 1
 
 # Eqn. (A2) of arXiv:1004.0342; u0 = 1.0
 const
@@ -46,32 +69,38 @@ const
   Cr = -1.0/20.0
 
 let 
-  # Read from file if ReadInputs = true
+  ## Brief: default inputs -- overriden if path to JSON provided (see newHISQHMC)
   defaultInputs = %* {
-    "lattice-geometry": [8,8,8,16],
+    "lattice-geometry": [8,8,8,16], # lattice geometry
+    #"rank-geometry": [1,1,2,2], # MPI rank geometry -- if not specified, guessed
+    #"simd-geometry": [2,2,1,2], # SIMD (local) geometry -- if not specified, guessed
     "hmc": {
-      "trajectory-length": 1.0,
-      "serial-rng": "milc",
-      "parallel-rng": "milc",
-      "serial-seed": 123456789,
-      "parallel-seed": 123456789,
-      "gauge-start": "cold"
+      "trajectory-length": 0.1, # HMC trajectory length
+      "serial-rng": "milc", # RNG for Metropolis
+      "parallel-rng": "milc", # RNG for heatbath
+      "serial-seed": 123456789, # Metropolis RNG seed
+      "parallel-seed": 123456789, # heatbath RNG seed
+      "gauge-start": "cold" # start -- hot, cold, or read; 
+                            # if read, need --ensemble flag (see newHISQHMC)
     },
     "action": {
-      "beta": 12.0,
-      "mass": 0.001,
-      "hasenbusch-mass": 0.2,
-      "lepage": 0.0,
-      "naik": 1.0,
-      "boundary-conditions": "pppa"
+      "beta": 7.5, # bare gauge coupling
+      "mass": 0.005, # degenerate fermion mass
+      "hasenbusch-mass": 0.6, # Hasnebusch mass
+      "lepage": 0.0, # lepage for fat7 (<asqtad fat7> = 2-<fat7 lepage>)
+      "naik": 1.0 # naik for fat7 (<asqtad naik> = 1.0)
     },
     "gauge": {
-      "integrator": "2MN",
-      "steps": 6
+      "integrator": "2MN", # outer fermion integrator (2nd-order Omelyan)
+      "steps": 10 # outer fermion steps
     },
     "fermion": {
-      "integrator": "2MN",
-      "steps": 2
+      "integrator": "2MN", # inner gauge integrator (2nd-order Omelyan)
+      "steps": 10 # gauge steps per outer gauge update
+    },
+    "measurements": {
+      "plaquette": {}, # tells isMainModule program to measure plaquette
+      "polyakov": {}, # tells isMainModule program to measure Polyakov loop
     }
   }
 
@@ -95,11 +124,11 @@ type
     hi,hf: float
     baseFilename: string
     trajs,traj0: int
-    integrator: ParIntegrator
+    integrator: Integrator
     srng*: SerialRNG
     prng*: ParallelRNG
     p,f: seq[U]
-  HisqHMC*[U,F,F0] = ref object of HisqHmcRoot[U]
+  HisqHMC*[U,F,F0] = ref object of HisqHMCRoot[U]
     beta,mass,hmass: float
     bcs: string
     u,u0: seq[U]
@@ -110,10 +139,11 @@ type
     params: HisqCoefs
     spa,spf: SolverParams
     perf: PerfInfo
+    jsonInfo*: JsonNode
 
-template UU(lo: Layout): untyped = 
+template UU(lo: Layout): untyped =
   type(lo.ColorMatrix())
-template FF(lo: Layout): untyped = 
+template FF(lo: Layout): untyped =
   type(lo.ColorVector())
 template FF0(lo: Layout): untyped = 
   type(lo.ColorVector()[0])
@@ -164,9 +194,9 @@ proc newSerialRNG*(generator: string; seed: int): SerialRng =
 
 #[ For construction of HisqHMC object ]#
 
-proc readJSON(fn: string): JsonNode = fn.parseFile
+proc readJSON*(fn: string): JsonNode = fn.parseFile
 
-proc readCMD: JsonNode = 
+proc readCMD*: JsonNode = 
   var cmd = initOptParser()
   result = parseJson("{}")
   while true:
@@ -216,10 +246,11 @@ proc newParallelRNG(lo: Layout; info: JsonNode): auto =
     info["hmc"]["parallel-seed"].getInt()
   )
 
-proc readGauge(u: auto; fn: string) =
+proc readGauge*(u: auto; fn: string) =
   if fileExists(fn):
     if 0 != u.loadGauge(fn): qexError "unable to read " & fn
     else: discard
+  else: qexError fn & " does not exist"
 
 proc readGauge*(self: var HisqHMC; fn: string) = self.u.readGauge(fn)
 
@@ -228,7 +259,7 @@ proc writeGauge[T](u: T; fn: string) =
 
 proc writeGauge*(self: var HisqHMC; fn: string) = self.u.writeGauge(fn)
 
-proc getIntSeq(input: JsonNode): seq[int] = 
+proc getIntSeq*(input: JsonNode): seq[int] = 
   result = newSeq[int]()
   for elem in input.getElems(): result.add elem.getInt()
 
@@ -250,27 +281,56 @@ proc `$`*(self: HisqHMC): string =
       mass: self.mass,
       hasenbusch_mass: self.hmass,
       cp: Cp,
-      cr: Cr,
-      boundary_conditions: self.bcs
+      cr: Cr
     )
   for tag,val in params.fieldPairs: 
     result = result & tag.replace("_"," ") & ": " & $(val) & "\n"
   result = result & $(self.params)
 
 template newHisqHMC*(build: untyped): auto =
+  ## Brief: HISQ HMC constructor template
+  ## Details:
+  ## Any program using this to construct the HISQ HMC object
+  ## for *production running* should provide the following flags.
+  ## --start: starting trajectory number
+  ## --ntraj: number of trajectories
+  ## --ensemble: default name for ensemble
+  ## --json: location of JSON file (see "defaultInputs" for template)
+  ## 
+  ## Note that serial & parallel RNG are checkpointed if "read" specified 
+  ## for "start" in JSON file.
   let 
+    # get information
     cmd = readCMD()
     info = case cmd.hasKey("json")
       of true: readJSON(cmd["json"].getStr())
       of false: defaultInputs
-    lo = newLayout(info["lattice-geometry"].getIntSeq())
+    
+    # create layout -- depends on user specifications
+    latLayout = info["lattice-geometry"].getIntSeq()
+    lo = case info.hasKey("rank-geometry")
+       of true:
+         case info.hasKey("simd-geometry"):
+           of true:
+             newLayout(
+               latLayout,
+               VLEN,
+               info["rank-geometry"].getIntSeq(),
+               info["simd-geometry"].getIntSeq()
+             )
+           of false: newLayout(latLayout,info["rank-geometry"].getIntSeq())
+       of false: newLayout(latLayout)
+
+    # variables injected to caller's template call
     fermionSteps {.inject.} = info["fermion"]["steps"].getInt()
     gaugeSteps {.inject.} = info["gauge"]["steps"].getInt()
     fermionIntegrator {.inject.} = info.setIntegrator("fermion")
     gaugeIntegrator {.inject.} = info.setIntegrator("gauge")
     start {.inject.} = info["hmc"]["gauge-start"].getStr()
+  
+  # modifiable variables inject to caller's template call
   var 
-    integrator {.inject.}: ParIntegrator
+    integrator {.inject.}: Integrator
     hisq {.inject.} = HisqHMC[lo.UU,lo.FF,lo.FF0]()
 
   # Prepare HMC
@@ -307,7 +367,7 @@ template newHisqHMC*(build: untyped): auto =
     beta,
     info["action"]["mass"].getFloat(),
     info["action"]["hasenbusch-mass"].getFloat(),
-    info["action"]["boundary-conditions"].getStr(),
+    "pppa",
     newHISQ(info),
     newSolverParams(info,"action"),
     newSolverParams(info,"force"),
@@ -342,6 +402,7 @@ template newHisqHMC*(build: untyped): auto =
   template u: untyped {.inject.} = hisq.u
   build
   hisq.integrator = integrator
+  hisq.jsonInfo = info
   hisq
 
 #[ Everything else... ]#
@@ -420,10 +481,15 @@ template rephase(g: auto) =
   g.stagPhase
 
 proc smearRephase(hisq: HisqCoefs; g: auto; sg,sgl: auto): auto {.discardable.} =
+  let 
+    displayPerformance = case SmearingVerbosity
+      of 0: false
+      of 1: true
+      else: true
   threads: g.rephase
-  let smearedForce = hisq.smearGetForce(g,sg,sgl)
+  let sf = hisq.smearGetForce(g,sg,sgl,displayPerformance=displayPerformance)
   threads: g.rephase
-  smearedForce
+  result = sf
 
 proc smear*(self: var HisqHMC) = 
   discard self.params.smearRephase(self.u,self.su,self.sul)
@@ -431,14 +497,16 @@ proc smear*(self: var HisqHMC) =
 proc smearGetForce*(self: var HisqHMC): auto =
   result = self.params.smearRephase(self.u,self.su,self.sul)
 
-proc kineticAction*[T](p: T): float =
+proc pnorm2[T](p: T): float =
   var p2: float
   threads:
     var p2t = 0.0
     for mu in 0..<p.len: p2t += p[mu].norm2
     threadBarrier()
     threadMaster: p2 = p2t
-  result = 0.5*p2 - 16.0*float(p[0].l.physVol)
+  result = p2
+
+proc kineticAction*[T](p: T): float = 0.5*p.pnorm2 - 16.0*float(p[0].l.physVol)
 
 proc fermionAction*(self: HisqHMC): float =
   var 
@@ -463,10 +531,7 @@ proc hamiltonian(self: HisqHMC): float =
       fermion: self.fermionAction()
     )
   var prnt = ""
-  for tag,val in h.fieldPairs: 
-    prnt = case tag
-      of "fermion": prnt & tag & " = " & $(val)
-      else: prnt & tag & " = " & $(val) & ", "
+  for tag,val in h.fieldPairs: prnt = prnt & tag & ": " & $(val) & " "
   echo prnt
   result = h.kinetic + h.gauge + h.fermion
 
@@ -491,14 +556,9 @@ proc fermionHeatbath*(self: var HisqHMC) =
   var 
     psi = self.u[0].l.ColorVector() 
     hpsi = self.u[0].l.ColorVector() 
-  self.prng.randomComplexGaussian(psi)
   self.prng.randomComplexGaussian(hpsi)
-  self.stag.pseudofermion(
-    self.phi,self.hphi,
-    psi,hpsi,
-    self.mass,self.hmass,
-    self.spa
-  )
+  self.prng.randomComplexGaussian(psi)
+  self.stag.pseudofermion(self.phi,self.hphi,psi,hpsi,self.mass,self.hmass,self.spa)
 
 proc prepare*(self: var HisqHMC) =
   self.backup()
@@ -517,20 +577,6 @@ proc revert(self: var HisqHMC) = set(self.u,self.u0)
 proc evolve*(self: var HisqHMC) = 
   self.integrator.evolve(self.tau)
   self.integrator.finish
-
-# Temporary fix to conflict between parallel layout & multiple shifts
-proc `^***`[T](shifter: auto; field: T): auto {.discardable.} = 
-  const n = field[0].len
-  var temp = field.newOneOf
-
-  proc replace() =
-    threads:
-      for s in field:
-        forO i, 0, n-1: temp[s][i] := shifter.field[s][i]
-
-  discard shifter ^* field
-  replace(); discard shifter ^* temp;
-  replace(); shifter ^* temp;
 
 proc fermionForce[S,T](
     f: auto; 
@@ -560,10 +606,6 @@ proc fermionForce[S,T](
     ht3[mu] = newShifter(hp,mu,3)
     discard t3[mu] ^* p
     discard ht3[mu] ^* hp
-    #t3[mu] = newShifter(p,mu,1)
-    #ht3[mu] = newShifter(hp,mu,1)
-    #discard t3[mu] ^*** p
-    #discard ht3[mu] ^*** hp
 
   # 1. Dslash
   const n = p[0].len
@@ -601,32 +643,65 @@ proc fermionForce[S,T](
     threadBarrier()
     g.rephase
 
-proc sq(input: float): float = input*input
+proc sq(x: float): float = x*x
 
 proc forceSolve[T](
     stag: auto; 
     psi: auto; 
     phi: T; 
     mass: float; 
-    sp: var SolverParams
+    sp0: var SolverParams
   ) =
-  var varphi = phi.l.ColorVector()
+  tic()
+  var
+    varphi = newOneOf(psi)
+    r = newOneOf(phi)
+    r2,b2: float
+    sp = sp0
+
+  # Prepare solver parameters
+  sp.resetStats()
+  dec sp.verbosity
+  sp.usePrevSoln = false
+  threads:
+    let b2t = phi.norm2
+    threadBarrier()
+    threadMaster: b2 = b2t
+
+  # Get solution
   stag.solveEE(varphi,phi,mass,sp)
   threads:
+    # Get residual
+    var r2t: float
+    stag.D(r,varphi,mass)
+    threadBarrier()
+    r2t = r.norm2
+    threadBarrier()
+    threadMaster: r2 = r2t
+
+    # Get solution for force
     varphi.even := 4.0*varphi
     threadBarrier()
     stagD2(stag.so,psi,stag.g,varphi,0,0)
     threadBarrier()
     psi.even := varphi
 
-proc fermionForce*(self: var HisqHMC) =
+  # Get information about solve
+  sp.r2.init r2/b2
+  sp.calls = 1
+  sp.seconds = getElapsedTime()
+  sp.flops += float((stag.g.len*4*72+24)*psi.l.nEven) # ???
+  if sp0.verbosity>0: echo "stagSolve: ", sp.getStats
+  sp0.addStats(sp)
+
+proc fermionForce*(self: var HisqHMC; dtau: float) =
   var 
     psi = self.u[0].l.ColorVector()
     hpsi = self.u[0].l.ColorVector()
   let 
     smearedForce = self.params.smearRephase(self.u,self.su,self.sul)
-    ffac = 0.25
-    hfac = 0.25*(self.hmass.sq-self.mass.sq)
+    ffac = 0.25*dtau
+    hfac = ffac*(self.hmass.sq-self.mass.sq)
   self.stag.forceSolve(psi,self.phi,self.hmass,self.spf)
   self.stag.forceSolve(hpsi,self.hphi,self.mass,self.spf)
   self.f.fermionForce(smearedForce,psi,hpsi,self.u,ffac,hfac)
@@ -647,12 +722,20 @@ proc updateMomentum[T](p: auto; f: T; dtau: float) =
     for mu in 0..<f.len: p[mu] -= dtau*f[mu]
   GC_fullCollect()
 
+proc updateMomentum[T](p: auto; f: T) =
+  threads:
+    for mu in 0..<f.len: p[mu] -= f[mu]
+  GC_fullCollect()
+
 proc updateMomentum*(self: var HisqHMC; dtau: float) =
   self.p.updateMomentum(self.f,dtau)
 
+proc updateMomentum*(self: var HisqHMC) =
+  self.p.updateMomentum(self.f)
+
 proc updateMomentumFermion*(self: var HisqHMC; dtau: float) =
-  self.fermionForce()
-  self.updateMomentum(dtau)
+  self.fermionForce(dtau)
+  self.updateMomentum()
 
 proc updateMomentumGauge*(self: var HisqHMC; dtau: float) =
   self.gaugeForce()
@@ -666,11 +749,18 @@ template finish*(self: var HisqHMC; input: untyped) =
   # Get information accessible to user
   var 
     info {.inject.} = (dH:0.0,expdH:0.0,rnd:0.0)
+    metropolis = true
     accepted {.inject.}: bool
   info.dH = self.hf - self.hi
   info.expdH = exp(-info.dH)
-  info.rnd = self.srng.uniform
-  accepted = info.rnd <= info.expdH
+  if self.jsonInfo["hmc"].hasKey("metropolis"):
+    metropolis = self.jsonInfo["hmc"]["metropolis"].getBool()
+  if metropolis:
+    info.rnd = self.srng.uniform
+    accepted = info.rnd <= info.expdH
+  else:
+    info.rnd = 0
+    accepted = true
   template u: untyped {.inject.} = self.u
 
   # Do metropolis & have user do what they will
@@ -698,16 +788,13 @@ proc fermionForceCheck*(self: var HisqHMC; eps: float): float =
   result = eps*contract(self.p,self.f)
 
 if isMainModule:
-  # This is an example of the kind of program
-  # that one could write using this code as a module
+  ## This is an example of the kind of program that one could write 
+  ## using this code as a module. It also runs on its own.
   qexInit()
   echo banner
 
   let 
     saveFreq = 5
-    measPlaq = true
-    measPoly = true
-    measCond = true
     baseFilename = "checkpoint"
 
   # Proc for calculating plaquette
@@ -730,23 +817,6 @@ if isMainModule:
       pls = pl[0..^2].sum / float(pl.len-1)
       plt = pl[^1]
     echo "MEASploop spatial: ",pls.re," ",pls.im," temporal: ",plt.re," ",plt.im
-  
-  # Proc for measuring chiral condensate
-  proc condensate(hmc: auto) =
-    var
-      pbpsp: SolverParams
-      tmpa = hmc.stag.g[0].l.ColorVector()
-      tmpb = hmc.stag.g[0].l.ColorVector()
-    let 
-      mass = hmc.mass
-      vol = hmc.stag.g[0].l.physVol.float
-    pbpsp.r2req = ActionCGTol
-    pbpsp.maxits = ActionMaxCGIter
-    threads: tmpa.u1(hmc.prng.milc)
-    hmc.stag.solve(tmpb,tmpa,mass,pbpsp)
-    threads:
-      let pbp = tmpb.norm2
-      threadMaster: echo "MEASpbp mass ",mass," : ",mass*pbp/vol
 
   # Construct HMC object
   var hmc = newHisqHMC:
@@ -759,21 +829,20 @@ if isMainModule:
       if (dtauG != 0.0): hisq.updateMomentumGauge(dtauG)
       if (dtauF != 0.0): hisq.updateMomentumFermion(dtauF)
 
-    # Construct integrator according to mdEvolve scheme
+    # Construct nested integrator
     let 
-      (VAll,T) = newIntegratorPair(mdvAll,mdt)
-      (V,Vf) = (VAll[0],VAll[1])
-    integrator = newParallelEvolution(
-      gaugeIntegrator(steps = gaugeSteps, V = V, T = T),
-      fermionIntegrator(steps = fermionSteps, V = Vf, T = T)
-    )
+      (V,T) = newIntegratorPair(mdvAll,mdt)
+      VG = gaugeIntegrator(steps = gaugeSteps, V = V[0], T = T)
+      VF = V[1]
+    integrator = fermionIntegrator(steps = fermionSteps, V = VF, T = VG)
 
     # Read information from disk
     if start == "read":
       let fn = baseFilename & "_" & $(hisq.traj0)
       hisq.readGauge(fn & ".lat")
-      hisq.readSerialRNG(fn & ".serialRNG")
-      hisq.readParallelRNG(fn & ".parallelRNG")
+      if hisq.traj0 > 0:
+        hisq.readSerialRNG(fn & ".serialRNG")
+        hisq.readParallelRNG(fn & ".parallelRNG")
       u.plaquette
       u.polyakov
       u.reunit
@@ -784,17 +853,19 @@ if isMainModule:
     hmc.prepare()
     hmc.evolve()
     hmc.finish:
-      let output = $(info.dH) & ", " & $(info.expdH) & ", " & $(info.rnd)
+      let output = $(info.dH) & " exp(dH): " & $(info.expdH) & " rand: " & $(info.rnd)
       case accepted:
         of true: echo "ACC: ", output
         of false: echo "REJ: ", output
-      if measPlaq: u.plaquette
-      if measPoly: u.polyakov
-      if measCond: hmc.condensate
-      if (saveFreq > 0) and (((trajectory + 1) mod saveFreq) == 0):
-        let fn = baseFilename & "_" & $(trajectory + 1)
-        hmc.writeGauge(fn & ".lat")
-        hmc.writeSerialRNG(fn & ".serialRNG")
-        hmc.writeParallelRNG(fn & ".parallelRNG")
+      if hmc.jsonInfo.hasKey("measurements"):
+        if hmc.jsonInfo["measurements"].hasKey("plaquette"): u.plaquette
+        if hmc.jsonInfo["measurements"].hasKey("polyakov"): u.polyakov
+      if hmc.jsonInfo.hasKey("checkpoint"):
+        let saveFreq = hmc.jsonInfo["checkpoint"]["frequency"].getInt()
+        if (saveFreq > 0) and (((trajectory + 1) mod saveFreq) == 0):
+          let fn = baseFilename & "_" & $(trajectory + 1)
+          hmc.writeGauge(fn & ".lat")
+          hmc.writeSerialRNG(fn & ".serialRNG")
+          hmc.writeParallelRNG(fn & ".parallelRNG")
 
   qexFinalize()
