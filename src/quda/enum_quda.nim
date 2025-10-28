d264170eea74895458b91a1358d566eeb72ed65a
Xiao-Yong Jin <xjin@anl.gov>
2025-10-28 16:17:26 -0700

    update quda wrappers

--- src/quda/enum_quda.nim
+++ src/quda/enum_quda.nim
@@ -1,12 +1,13 @@
 const
-  QUDA_INVALID_ENUM* = low(cint)
+  QUDA_INVALID_ENUM* = (-0x7fffffff - 1)
 
 type
   qudaError_t* {.size: sizeof(cint).} = enum
     QUDA_SUCCESS = 0, QUDA_ERROR = 1, QUDA_ERROR_UNINITIALIZED = 2
   QudaMemoryType* {.size: sizeof(cint).} = enum
-    QUDA_MEMORY_INVALID = QUDA_INVALID_ENUM, QUDA_MEMORY_DEVICE = 0, QUDA_MEMORY_PINNED,
-    QUDA_MEMORY_MAPPED
+    QUDA_MEMORY_INVALID = QUDA_INVALID_ENUM, QUDA_MEMORY_DEVICE = 0,
+    QUDA_MEMORY_DEVICE_PINNED, QUDA_MEMORY_HOST, QUDA_MEMORY_HOST_PINNED,
+    QUDA_MEMORY_MAPPED, QUDA_MEMORY_MANAGED
 
 
 
@@ -18,12 +19,10 @@ type
   QudaLinkType* {.size: sizeof(cint).} = enum
     QUDA_INVALID_LINKS = QUDA_INVALID_ENUM, QUDA_SU3_LINKS = 0, QUDA_GENERAL_LINKS,
     QUDA_THREE_LINKS, QUDA_MOMENTUM_LINKS, QUDA_COARSE_LINKS, ##  used for coarse-gauge field with multigrid
-    QUDA_SMEARED_LINKS        ##  used for loading and saving gaugeSmeared in the interface
+    QUDA_SMEARED_LINKS,       ##  used for loading and saving gaugeSmeared in the interface
+    QUDA_TWOLINK_LINKS        ##  used for staggered fermion smearing.
   QudaGaugeFieldOrder* {.size: sizeof(cint).} = enum
-    QUDA_INVALID_GAUGE_ORDER = QUDA_INVALID_ENUM, QUDA_FLOAT_GAUGE_ORDER = 1, QUDA_FLOAT2_GAUGE_ORDER = 2, ##  no reconstruct and double precision
-    QUDA_FLOAT4_GAUGE_ORDER = 4, ##  8 reconstruct single, and 12 reconstruct single, half, quarter
-    QUDA_FLOAT8_GAUGE_ORDER = 8, ##  8 reconstruct half and quarter
-    QUDA_NATIVE_GAUGE_ORDER,  ##  used to denote one of the above types in a trait, not used directly
+    QUDA_INVALID_GAUGE_ORDER = QUDA_INVALID_ENUM, QUDA_NATIVE_GAUGE_ORDER = 0, ##  used to denote the internal QUDA ordering
     QUDA_QDP_GAUGE_ORDER,     ##  expect *gauge[mu], even-odd, spacetime, row-column color
     QUDA_QDPJIT_GAUGE_ORDER,  ##  expect *gauge[mu], even-odd, complex-column-row-spacetime
     QUDA_CPS_WILSON_GAUGE_ORDER, ##  expect *gauge, even-odd, mu, spacetime, column-row color
@@ -64,6 +63,8 @@ const
 ##
 ##  Types used in QudaInvertParam
 ##
+##  Note: make sure QudaDslashType has corresponding entries in
+##  tests/utils/misc.cpp
 
 type
   QudaDslashType* {.size: sizeof(cint).} = enum
@@ -75,24 +76,24 @@ type
     QUDA_COVDEV_DSLASH
   QudaInverterType* {.size: sizeof(cint).} = enum
     QUDA_INVALID_INVERTER = QUDA_INVALID_ENUM, QUDA_CG_INVERTER = 0, QUDA_BICGSTAB_INVERTER,
-    QUDA_GCR_INVERTER, QUDA_MR_INVERTER, QUDA_MPBICGSTAB_INVERTER,
-    QUDA_SD_INVERTER, QUDA_PCG_INVERTER, QUDA_MPCG_INVERTER, QUDA_EIGCG_INVERTER,
-    QUDA_INC_EIGCG_INVERTER, QUDA_GMRESDR_INVERTER, QUDA_GMRESDR_PROJ_INVERTER,
-    QUDA_GMRESDR_SH_INVERTER, QUDA_FGMRESDR_INVERTER, QUDA_MG_INVERTER,
-    QUDA_BICGSTABL_INVERTER, QUDA_CGNE_INVERTER, QUDA_CGNR_INVERTER,
-    QUDA_CG3_INVERTER, QUDA_CG3NE_INVERTER, QUDA_CG3NR_INVERTER,
-    QUDA_CA_CG_INVERTER, QUDA_CA_CGNE_INVERTER, QUDA_CA_CGNR_INVERTER,
-    QUDA_CA_GCR_INVERTER
+    QUDA_GCR_INVERTER, QUDA_MR_INVERTER, QUDA_SD_INVERTER, QUDA_PCG_INVERTER,
+    QUDA_EIGCG_INVERTER, QUDA_INC_EIGCG_INVERTER, QUDA_GMRESDR_INVERTER,
+    QUDA_GMRESDR_PROJ_INVERTER, QUDA_GMRESDR_SH_INVERTER, QUDA_FGMRESDR_INVERTER,
+    QUDA_MG_INVERTER, QUDA_BICGSTABL_INVERTER, QUDA_CGNE_INVERTER,
+    QUDA_CGNR_INVERTER, QUDA_CG3_INVERTER, QUDA_CG3NE_INVERTER,
+    QUDA_CG3NR_INVERTER, QUDA_CA_CG_INVERTER, QUDA_CA_CGNE_INVERTER,
+    QUDA_CA_CGNR_INVERTER, QUDA_CA_GCR_INVERTER
   QudaEigType* {.size: sizeof(cint).} = enum
     QUDA_EIG_INVALID = QUDA_INVALID_ENUM, QUDA_EIG_TR_LANCZOS = 0, ##  Thick restarted lanczos solver
     QUDA_EIG_BLK_TR_LANCZOS,  ##  Block Thick restarted lanczos solver
+    QUDA_EIG_TR_LANCZOS_3D,   ##  Thick restarted lanczos solver for 3-d systems
     QUDA_EIG_IR_ARNOLDI,      ##  Implicitly Restarted Arnoldi solver
     QUDA_EIG_BLK_IR_ARNOLDI   ##  Block Implicitly Restarted Arnoldi solver
 
 
 
 
-## * S=smallest L=largest
+##  S=smallest L=largest
 ##     R=real M=modulus I=imaniary *
 
 type
@@ -109,11 +110,13 @@ type
     QUDA_DIRECT_PC_SOLVE, QUDA_NORMOP_PC_SOLVE, QUDA_NORMERR_SOLVE,
     QUDA_NORMERR_PC_SOLVE
   QudaMultigridCycleType* {.size: sizeof(cint).} = enum
-    QUDA_MG_CYCLE_INVALID = QUDA_INVALID_ENUM, QUDA_MG_CYCLE_VCYCLE = 0,
-    QUDA_MG_CYCLE_FCYCLE, QUDA_MG_CYCLE_WCYCLE, QUDA_MG_CYCLE_RECURSIVE
+    QUDA_MG_CYCLE_INVALID = QUDA_INVALID_ENUM, QUDA_MG_CYCLE_VCYCLE = 0, QUDA_MG_CYCLE_FCYCLE,
+    QUDA_MG_CYCLE_WCYCLE, QUDA_MG_CYCLE_RECURSIVE
   QudaSchwarzType* {.size: sizeof(cint).} = enum
     QUDA_INVALID_SCHWARZ = QUDA_INVALID_ENUM, QUDA_ADDITIVE_SCHWARZ = 0,
-    QUDA_MULTIPLICATIVE_SCHWARZ
+    QUDA_MULTIPLICATIVE_SCHWARZ = 1
+  QudaAcceleratorType* {.size: sizeof(cint).} = enum
+    QUDA_INVALID_ACCELERATOR = QUDA_INVALID_ENUM, QUDA_MADWF_ACCELERATOR = 0 ##  Use the MADWF accelerator
   QudaResidualType* {.size: sizeof(cint).} = enum
     QUDA_INVALID_RESIDUAL = QUDA_INVALID_ENUM, QUDA_L2_RELATIVE_RESIDUAL = 1, ##  L2 relative residual (default)
     QUDA_L2_ABSOLUTE_RESIDUAL = 2, ##  L2 absolute residual
@@ -124,6 +127,7 @@ type
 const
   QUDA_NORMEQ_SOLVE* = QUDA_NORMOP_SOLVE
   QUDA_NORMEQ_PC_SOLVE* = QUDA_NORMOP_PC_SOLVE
+
 
 
 
@@ -169,21 +173,16 @@ type
     QUDA_LEX_DIRAC_ORDER,     ##  lexicographical order, color inside spin
     QUDA_TIFR_PADDED_DIRAC_ORDER ##  padded z dimension for TIFR RHMC code
   QudaCloverFieldOrder* {.size: sizeof(cint).} = enum
-    QUDA_INVALID_CLOVER_ORDER = QUDA_INVALID_ENUM, QUDA_FLOAT_CLOVER_ORDER = 1, ##  even-odd float ordering
-    QUDA_FLOAT2_CLOVER_ORDER = 2, ##  even-odd float2 ordering
-    QUDA_FLOAT4_CLOVER_ORDER = 4, ##  even-odd float4 ordering
+    QUDA_INVALID_CLOVER_ORDER = QUDA_INVALID_ENUM, QUDA_NATIVE_CLOVER_ORDER = 0, ##  even-odd, Fload-N ordering
     QUDA_PACKED_CLOVER_ORDER, ##  even-odd, QDP packed
     QUDA_QDPJIT_CLOVER_ORDER, ##  (diagonal / off-diagonal)-chirality-spacetime
     QUDA_BQCD_CLOVER_ORDER    ##  even-odd, super-diagonal packed and reordered
   QudaVerbosity* {.size: sizeof(cint).} = enum
-    QUDA_INVALID_VERBOSITY = QUDA_INVALID_ENUM, QUDA_SILENT = 0, QUDA_SUMMARIZE,
-    QUDA_VERBOSE, QUDA_DEBUG_VERBOSE
-  QudaTune* {.size: sizeof(cint).} = enum
-    QUDA_TUNE_INVALID = QUDA_INVALID_ENUM, QUDA_TUNE_NO = 0, QUDA_TUNE_YES
+    QUDA_INVALID_VERBOSITY = QUDA_INVALID_ENUM, QUDA_SILENT = 0, QUDA_SUMMARIZE, QUDA_VERBOSE,
+    QUDA_DEBUG_VERBOSE
   QudaPreserveDirac* {.size: sizeof(cint).} = enum
     QUDA_PRESERVE_DIRAC_INVALID = QUDA_INVALID_ENUM, QUDA_PRESERVE_DIRAC_NO = 0,
     QUDA_PRESERVE_DIRAC_YES
-
 
 
 
@@ -210,9 +209,9 @@ type
 type
   QudaDiracType* {.size: sizeof(cint).} = enum
     QUDA_INVALID_DIRAC = QUDA_INVALID_ENUM, QUDA_WILSON_DIRAC = 0, QUDA_WILSONPC_DIRAC,
-    QUDA_CLOVER_DIRAC, QUDA_CLOVER_HASENBUSCH_TWIST_DIRAC,
-    QUDA_CLOVER_HASENBUSCH_TWISTPC_DIRAC, QUDA_CLOVERPC_DIRAC,
-    QUDA_DOMAIN_WALL_DIRAC, QUDA_DOMAIN_WALLPC_DIRAC, QUDA_DOMAIN_WALL_4D_DIRAC,
+    QUDA_CLOVER_DIRAC, QUDA_CLOVERPC_DIRAC, QUDA_CLOVER_HASENBUSCH_TWIST_DIRAC,
+    QUDA_CLOVER_HASENBUSCH_TWISTPC_DIRAC, QUDA_DOMAIN_WALL_DIRAC,
+    QUDA_DOMAIN_WALLPC_DIRAC, QUDA_DOMAIN_WALL_4D_DIRAC,
     QUDA_DOMAIN_WALL_4DPC_DIRAC, QUDA_MOBIUS_DOMAIN_WALL_DIRAC,
     QUDA_MOBIUS_DOMAIN_WALLPC_DIRAC, QUDA_MOBIUS_DOMAIN_WALL_EOFA_DIRAC,
     QUDA_MOBIUS_DOMAIN_WALLPC_EOFA_DIRAC, QUDA_STAGGERED_DIRAC,
@@ -250,32 +249,43 @@ type
 
 ##  Degree of freedom ordering
 
-type
+type                          ##  gamj=((top 2 rows)(bottom 2 rows))  s1,s2,s3 are Pauli spin matrices, 1 is 2x2 identity
   QudaFieldOrder* {.size: sizeof(cint).} = enum
-    QUDA_INVALID_FIELD_ORDER = QUDA_INVALID_ENUM, QUDA_FLOAT_FIELD_ORDER = 1, ##  spin-color-complex-space
-    QUDA_FLOAT2_FIELD_ORDER = 2, ##  (spin-color-complex)/2-space-(spin-color-complex)%2
-    QUDA_FLOAT4_FIELD_ORDER = 4, ##  (spin-color-complex)/4-space-(spin-color-complex)%4
-    QUDA_FLOAT8_FIELD_ORDER = 8, ##  (spin-color-complex)/8-space-(spin-color-complex)%8
+    QUDA_INVALID_FIELD_ORDER = QUDA_INVALID_ENUM, QUDA_NATIVE_FIELD_ORDER = 0, ##  spin-color-complex-space
     QUDA_SPACE_SPIN_COLOR_FIELD_ORDER, ##  CPS/QDP++ ordering
     QUDA_SPACE_COLOR_SPIN_FIELD_ORDER, ##  QLA ordering (spin inside color)
     QUDA_QDPJIT_FIELD_ORDER,  ##  QDP field ordering (complex-color-spin-spacetime)
     QUDA_QOP_DOMAIN_WALL_FIELD_ORDER, ##  QOP domain-wall ordering
     QUDA_PADDED_SPACE_SPIN_COLOR_FIELD_ORDER ##  TIFR RHMC ordering
   QudaFieldCreate* {.size: sizeof(cint).} = enum
-    QUDA_INVALID_FIELD_CREATE = QUDA_INVALID_ENUM, QUDA_NULL_FIELD_CREATE = 0, ##  create new field
-    QUDA_ZERO_FIELD_CREATE,   ##  create new field and zero it
-    QUDA_COPY_FIELD_CREATE,   ##  create copy to field
-    QUDA_REFERENCE_FIELD_CREATE ##  create reference to field
+    QUDA_INVALID_FIELD_CREATE = QUDA_INVALID_ENUM, QUDA_NULL_FIELD_CREATE = 0, ##  new field
+    QUDA_ZERO_FIELD_CREATE,   ##  new field and zero it
+    QUDA_COPY_FIELD_CREATE,   ##  copy to field
+    QUDA_REFERENCE_FIELD_CREATE, ##  reference to field
+    QUDA_GHOST_FIELD_CREATE   ##  dummy field used only for ghost storage
   QudaGammaBasis* {.size: sizeof(cint).} = enum
-    QUDA_INVALID_GAMMA_BASIS = QUDA_INVALID_ENUM, QUDA_DEGRAND_ROSSI_GAMMA_BASIS = 0,
-    QUDA_UKQCD_GAMMA_BASIS, QUDA_CHIRAL_GAMMA_BASIS
+    QUDA_INVALID_GAMMA_BASIS = QUDA_INVALID_ENUM, QUDA_DEGRAND_ROSSI_GAMMA_BASIS = 0, ##  gam1=((0,i*s1)(-i*s1,0)) gam2=((0,-i*s2)(i*s2,0)) gam3=((0,i*s3)(-i*s3,0)) gam4=((0,1)(1,0))  gam5=((1,0)(0,-1))
+    QUDA_UKQCD_GAMMA_BASIS,   ##  gam1=((0,i*s1)(-i*s1,0)) gam2=((0,i*s2)(-i*s2,0)) gam3=((0,i*s3)(-i*s3,0)) gam4=((1,0)(0,-1)) gam5=((0,1)(1,0))
+    QUDA_CHIRAL_GAMMA_BASIS,  ##  gam1=((0,-i*s1)(i*s1,0)) gam2=((0,-i*s2)(i*s2,0)) gam3=((0,-i*s3)(i*s3,0)) gam4=((0,-1)(-1,0))gam5=((-1,0)(0,1))
+    QUDA_DIRAC_PAULI_GAMMA_BASIS ##  gam1=((0,-i*s1)(i*s1,0)) gam2=((0,-i*s2)(i*s2,0)) gam3=((0,-i*s3)(i*s3,0)) gam4=((1,0)(0,-1)) gam5=((0,-1)(-1,0))
+
+
+
+
+##   Dirac-Pauli -> DeGrand-Rossi   T = i/sqrt(2)*((s2,-s2)(s2,s2))     field_DR = T * field_DP
+##   UKQCD       -> DeGrand-Rossi   T = i/sqrt(2)*((-s2,-s2)(-s2,s2))   field_DR = T * field_UK
+##   Chiral      -> DeGrand-Rossi   T = i*((0,-s2)(s2,0))               field_DR = T * field_chiral
+
+type
   QudaSourceType* {.size: sizeof(cint).} = enum
     QUDA_INVALID_SOURCE = QUDA_INVALID_ENUM, QUDA_POINT_SOURCE = 0, QUDA_RANDOM_SOURCE,
     QUDA_CONSTANT_SOURCE, QUDA_SINUSOIDAL_SOURCE, QUDA_CORNER_SOURCE
   QudaNoiseType* {.size: sizeof(cint).} = enum
     QUDA_NOISE_INVALID = QUDA_INVALID_ENUM, QUDA_NOISE_GAUSS = 0, QUDA_NOISE_UNIFORM
-
-
+  QudaDilutionType* {.size: sizeof(cint).} = enum
+    QUDA_DILUTION_INVALID = QUDA_INVALID_ENUM, QUDA_DILUTION_SPIN = 0, QUDA_DILUTION_COLOR,
+    QUDA_DILUTION_SPIN_COLOR, QUDA_DILUTION_SPIN_COLOR_EVEN_ODD,
+    QUDA_DILUTION_BLOCK
 
 
 
@@ -294,8 +304,8 @@ type
   QudaPCType* {.size: sizeof(cint).} = enum
     QUDA_PC_INVALID = QUDA_INVALID_ENUM, QUDA_4D_PC = 4, QUDA_5D_PC = 5
   QudaTwistFlavorType* {.size: sizeof(cint).} = enum
-    QUDA_TWIST_INVALID = QUDA_INVALID_ENUM, QUDA_TWIST_DEG_DOUBLET = -2, QUDA_TWIST_NO = 0,
-    QUDA_TWIST_SINGLET = 1, QUDA_TWIST_NONDEG_DOUBLET = +2
+    QUDA_TWIST_INVALID = QUDA_INVALID_ENUM, QUDA_TWIST_NO = 0, QUDA_TWIST_SINGLET = 1,
+    QUDA_TWIST_NONDEG_DOUBLET = +2
   QudaTwistDslashType* {.size: sizeof(cint).} = enum
     QUDA_DSLASH_INVALID = QUDA_INVALID_ENUM, QUDA_DEG_TWIST_INV_DSLASH = 0,
     QUDA_DEG_DSLASH_TWIST_INV, QUDA_DEG_DSLASH_TWIST_XPAY, QUDA_NONDEG_DSLASH
@@ -312,14 +322,15 @@ type
     QUDA_DEFLATED_GUESS_INVALID = QUDA_INVALID_ENUM, QUDA_DEFLATED_GUESS_NO = 0,
     QUDA_DEFLATED_GUESS_YES
   QudaComputeNullVector* {.size: sizeof(cint).} = enum
-    QUDA_COMPUTE_NULL_VECTOR_INVALID = QUDA_INVALID_ENUM,
-    QUDA_COMPUTE_NULL_VECTOR_NO = 0, QUDA_COMPUTE_NULL_VECTOR_YES
+    QUDA_COMPUTE_NULL_VECTOR_INVALID = QUDA_INVALID_ENUM, QUDA_COMPUTE_NULL_VECTOR_NO = 0,
+    QUDA_COMPUTE_NULL_VECTOR_YES
   QudaSetupType* {.size: sizeof(cint).} = enum
     QUDA_INVALID_SETUP_TYPE = QUDA_INVALID_ENUM, QUDA_NULL_VECTOR_SETUP = 0,
     QUDA_TEST_VECTOR_SETUP
   QudaTransferType* {.size: sizeof(cint).} = enum
     QUDA_TRANSFER_INVALID = QUDA_INVALID_ENUM, QUDA_TRANSFER_AGGREGATE = 0,
-    QUDA_TRANSFER_COARSE_KD, QUDA_TRANSFER_OPTIMIZED_KD
+    QUDA_TRANSFER_COARSE_KD, QUDA_TRANSFER_OPTIMIZED_KD,
+    QUDA_TRANSFER_OPTIMIZED_KD_DROP_LONG
   QudaBoolean* {.size: sizeof(cint).} = enum
     QUDA_BOOLEAN_INVALID = QUDA_INVALID_ENUM, QUDA_BOOLEAN_FALSE = 0, QUDA_BOOLEAN_TRUE = 1
 
@@ -341,6 +352,8 @@ const
   QUDA_BOOLEAN_YES* = QUDA_BOOLEAN_TRUE
 
 type
+  QudaBLASType* {.size: sizeof(cint).} = enum
+    QUDA_BLAS_INVALID = QUDA_INVALID_ENUM, QUDA_BLAS_GEMM = 0, QUDA_BLAS_LU_INV = 1
   QudaBLASOperation* {.size: sizeof(cint).} = enum
     QUDA_BLAS_OP_INVALID = QUDA_INVALID_ENUM, QUDA_BLAS_OP_N = 0, ##  No transpose
     QUDA_BLAS_OP_T = 1,         ##  Transpose only
@@ -359,17 +372,36 @@ type
     QUDA_LINK_BACKWARDS = 0, QUDA_LINK_FORWARDS, QUDA_LINK_BIDIRECTIONAL
   QudaFieldGeometry* {.size: sizeof(cint).} = enum
     QUDA_INVALID_GEOMETRY = QUDA_INVALID_ENUM, QUDA_SCALAR_GEOMETRY = 1,
-    QUDA_VECTOR_GEOMETRY = 4, QUDA_TENSOR_GEOMETRY = 6, QUDA_COARSE_GEOMETRY = 8
+    QUDA_VECTOR_GEOMETRY = 4, QUDA_TENSOR_GEOMETRY = 6, QUDA_COARSE_GEOMETRY = 8, QUDA_KDINVERSE_GEOMETRY = 16 ##  Decomposition of Kahler-Dirac block
   QudaGhostExchange* {.size: sizeof(cint).} = enum
     QUDA_GHOST_EXCHANGE_INVALID = QUDA_INVALID_ENUM, QUDA_GHOST_EXCHANGE_NO = 0,
     QUDA_GHOST_EXCHANGE_PAD, QUDA_GHOST_EXCHANGE_EXTENDED
   QudaStaggeredPhase* {.size: sizeof(cint).} = enum
     QUDA_STAGGERED_PHASE_INVALID = QUDA_INVALID_ENUM, QUDA_STAGGERED_PHASE_NO = 0,
-    QUDA_STAGGERED_PHASE_MILC = 1, QUDA_STAGGERED_PHASE_CPS = 2,
+    QUDA_STAGGERED_PHASE_MILC = 1, QUDA_STAGGERED_PHASE_CHROMA = 2,
     QUDA_STAGGERED_PHASE_TIFR = 3
+  QudaSpinTasteGamma* {.size: sizeof(cint).} = enum
+    QUDA_SPIN_TASTE_INVALID = QUDA_INVALID_ENUM, QUDA_SPIN_TASTE_G1 = 0,
+    QUDA_SPIN_TASTE_GX = 1, QUDA_SPIN_TASTE_GY = 2, QUDA_SPIN_TASTE_GXGY = 3,
+    QUDA_SPIN_TASTE_GZ = 4, QUDA_SPIN_TASTE_GZGX = 5, QUDA_SPIN_TASTE_GYGZ = 6,
+    QUDA_SPIN_TASTE_G5GT = 7, QUDA_SPIN_TASTE_GT = 8, QUDA_SPIN_TASTE_GXGT = 9,
+    QUDA_SPIN_TASTE_GYGT = 10, QUDA_SPIN_TASTE_G5GZ = 11, QUDA_SPIN_TASTE_GZGT = 12,
+    QUDA_SPIN_TASTE_G5GY = 13, QUDA_SPIN_TASTE_G5GX = 14, QUDA_SPIN_TASTE_G5 = 15
   QudaContractType* {.size: sizeof(cint).} = enum
-    QUDA_CONTRACT_TYPE_INVALID = QUDA_INVALID_ENUM, QUDA_CONTRACT_TYPE_OPEN = 0, ##  Open spin elementals
-    QUDA_CONTRACT_TYPE_DR     ##  DegrandRossi
+    QUDA_CONTRACT_TYPE_INVALID = QUDA_INVALID_ENUM, QUDA_CONTRACT_TYPE_STAGGERED_FT_T = 0, ##  Staggered, FT in tdim
+    QUDA_CONTRACT_TYPE_DR_FT_T, ##  DegrandRossi insertion, FT in tdim
+    QUDA_CONTRACT_TYPE_DR_FT_Z, ##  DegrandRossi insertion, FT in zdim
+    QUDA_CONTRACT_TYPE_STAGGERED, ##  Staggered, no summation (TODO: remove line)
+    QUDA_CONTRACT_TYPE_DR,    ##  DegrandRossi insertion, no summation
+    QUDA_CONTRACT_TYPE_OPEN,  ##  Open spin elementals, no summation
+    QUDA_CONTRACT_TYPE_OPEN_SUM_T, ##  Open spin elementals, spatially summed over tdim
+    QUDA_CONTRACT_TYPE_OPEN_SUM_Z, ##  Open spin elementals, spatially summed over zdim
+    QUDA_CONTRACT_TYPE_OPEN_FT_T, ##  Open spin elementals, FT in tdim
+    QUDA_CONTRACT_TYPE_OPEN_FT_Z ##  Open spin elementals, FT in zdim
+  QudaFFTSymmType* {.size: sizeof(cint).} = enum
+    QUDA_FFT_SYMM_INVALID = QUDA_INVALID_ENUM, QUDA_FFT_SYMM_ODD = 1, ##  sin(phase)
+    QUDA_FFT_SYMM_EVEN = 2,     ##  cos(phase)
+    QUDA_FFT_SYMM_EO = 3        ##  exp(-i phase)
   QudaContractGamma* {.size: sizeof(cint).} = enum
     QUDA_CONTRACT_GAMMA_INVALID = QUDA_INVALID_ENUM, QUDA_CONTRACT_GAMMA_I = 0,
     QUDA_CONTRACT_GAMMA_G1 = 1, QUDA_CONTRACT_GAMMA_G2 = 2,
@@ -380,9 +412,21 @@ type
     QUDA_CONTRACT_GAMMA_S13 = 11, QUDA_CONTRACT_GAMMA_S14 = 12,
     QUDA_CONTRACT_GAMMA_S21 = 13, QUDA_CONTRACT_GAMMA_S23 = 14,
     QUDA_CONTRACT_GAMMA_S34 = 15
+  QudaGaugeSmearType* {.size: sizeof(cint).} = enum
+    QUDA_GAUGE_SMEAR_INVALID = QUDA_INVALID_ENUM, QUDA_GAUGE_SMEAR_APE = 0,
+    QUDA_GAUGE_SMEAR_STOUT, QUDA_GAUGE_SMEAR_OVRIMP_STOUT, QUDA_GAUGE_SMEAR_HYP,
+    QUDA_GAUGE_SMEAR_WILSON_FLOW, QUDA_GAUGE_SMEAR_SYMANZIK_FLOW
   QudaWFlowType* {.size: sizeof(cint).} = enum
     QUDA_WFLOW_TYPE_INVALID = QUDA_INVALID_ENUM, QUDA_WFLOW_TYPE_WILSON = 0,
     QUDA_WFLOW_TYPE_SYMANZIK
+  QudaFermionSmearType* {.size: sizeof(cint).} = enum
+    QUDA_FERMION_SMEAR_TYPE_INVALID = QUDA_INVALID_ENUM,
+    QUDA_FERMION_SMEAR_TYPE_GAUSSIAN = 0, QUDA_FERMION_SMEAR_TYPE_WUPPERTAL
+
+
+
+
+
 
 
 
@@ -399,6 +443,24 @@ type
 
 type
   QudaExtLibType* {.size: sizeof(cint).} = enum
-    QUDA_EXTLIB_INVALID = QUDA_INVALID_ENUM, QUDA_CUSOLVE_EXTLIB = 0, QUDA_EIGEN_EXTLIB,
-    QUDA_MAGMA_EXTLIB
+    QUDA_EXTLIB_INVALID = QUDA_INVALID_ENUM, QUDA_CUSOLVE_EXTLIB = 0, QUDA_EIGEN_EXTLIB
+  QudaDDType* {.size: sizeof(cint).} = enum
+    QUDA_DD_INVALID = QUDA_INVALID_ENUM, QUDA_DD_NO = 0, QUDA_DD_RED_BLACK
+  QudaWFlowStepType* {.size: sizeof(cint).} = enum
+    WFLOW_STEP_W1, WFLOW_STEP_W2, WFLOW_STEP_VT, WFLOW_FOURTH_ORDER_STEP_1,
+    WFLOW_FOURTH_ORDER_STEP_2, WFLOW_FOURTH_ORDER_STEP_3,
+    WFLOW_FOURTH_ORDER_STEP_4, WFLOW_FOURTH_ORDER_STEP_5,
+    WFLOW_FOURTH_ORDER_STEP_6
 
+
+
+
+##  Used by update_split_gauge
+
+type
+  QudaUpdateSplitGauge* {.size: sizeof(cint).} = enum
+    QUDA_UPDATE_SPLIT_GAUGE_FALSE = 0, ##  the input gauge fields will not be split and the buffered (split) gauges will be used for split grid solves
+    QUDA_UPDATE_SPLIT_GAUGE_TRUE = 1 ## the input gauge fields will be split and the buffered (split) gauges will be updated accordingly
+
+const
+    QUDA_UPDATE_SPLIT_GAUGE_OFF = QUDA_UPDATE_SPLIT_GAUGE_FALSE ##  will not use split gauge buffers
--- src/quda/makeWrapper.sh
+++ src/quda/makeWrapper.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
-c2n="c2nim"
+#c2n="c2nim"
+c2n=/g/g91/jin8/.nimble/bin/c2nim
 #c2n="/home/josborn/work/lqcd/src/c2nim-mygit/c2nim"
 #QUDA=$HOME/lqcd/src/quda-0.8.0
-QUDA=$HOME/work/lqcd/src/quda-git
+#QUDA=$HOME/work/lqcd/src/quda-git
+QUDA=/p/lustre5/jin8/soft.20251023.rocm6.4.2/quda
 QI=$QUDA/include
 
-ezero=`grep -A1 enum $QI/enum_quda.h |grep -v enum |grep -v = |grep QUDA |sed 's/,.*//' |sort`
-#echo $ezero
-szero=`echo "$ezero" |sed 's|\(QUDA_[A-Z0-9_]*\)|s/\1/\1=0/;|'`
+ezero=`grep -A1 enum $QI/enum_quda.h |grep -v '^ *typedef.*enum' |sed 's,\(//\|/\*\).*,,' |grep -v = |sed -n '/QUDA/{s/^ *//;s/,.*//;p}' |sort`
+ezero1=`sed -n '/enum.*{.*}.*;/{s/.*typedef.*enum.*{ *//; s/ *[,}].*//; s,\(//\|/\*\).*,,; p}' $QI/enum_quda.h |grep -v = |sort`
+#echo "$ezero"
+#echo "$ezero1"
+szero=`{ echo "$ezero" ; echo "$ezero1" ; } |sed 's|\(QUDA_[A-Z0-9_]*\)|s/\1\\\([^_a-zA-Z0-9]\\\)/\1 = 0\\\\1/;|'`
 echo $szero
 
 cat $QI/enum_quda.h |\
@@ -18,6 +22,11 @@ cat enum_quda.cnim2 |\
     sed 's/= -2147483647/= QUDA_INVALID_ENUM/' |
     sed "$szero" >enum_quda_new.nim
 rm enum_quda.cnim enum_quda.cnim2
+echo 'WARNING:
+	check QudaUpdateSplitGauge in enum_quda.nim
+	make sure to have
+	const QUDA_UPDATE_SPLIT_GAUGE_OFF = QUDA_UPDATE_SPLIT_GAUGE_FALSE
+	or something the same as in enum_quda.h'
 
 $c2n --header -o:quda_constants_new.nim quda.c2nim $QI/quda_constants.h
 
@@ -31,6 +40,17 @@ import enum_quda, quda_constants
 EOF
 cat quda.cnim2 >>quda_new.nim
 rm quda.cnim quda.cnim2
+echo 'WARNING:
+	check quda.nim
+	make sure to have
+	type
+	  ConstInt* {.importc:"const int".} = cint
+	  double_complex* {.importc:"double _Complex".} = object
+	converter toDoubleComplex*(x: array[2,float]): double_complex =
+	  var r = cast[ptr array[2,float]](addr result)
+	  r[] = x
+	type
+	  QudaCommsMap* = proc (coords: ptr cint; fdata: pointer): cint {.cdecl.}'
 
 
 exit
