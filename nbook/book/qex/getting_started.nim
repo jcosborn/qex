606c5e01c6c5c3095716f63d2fd7562db9c49fc9
James Osborn <osborn@alcf.anl.gov>
2026-04-17 23:53:25 -0500

    update nbook

--- nbook/book/qex/getting_started.nim
+++ nbook/book/qex/getting_started.nim
@@ -7,19 +7,7 @@ nbText: """
 ## QEX Getting Started
 
 This section demonstrates how to set up QEX in Nim.
+...
 """
-
-# #[
-nbCode:
-  import qex
-  qexInit()
-  let lat = [4, 4, 4, 4]
-  let lo = newLayout(lat)
-  var v = lo.ColorVector()
-  v := 1
-  if myRank == 0:
-    echo "Vector element at site 0:", v[0][0]
-  qexFinalize()
-# ]#
 
 nbSave
--- nbook/nbook.nim
+++ nbook/nbook.nim
@@ -5,7 +5,8 @@ var book = initBookWithToc:
   entry("Installation", "install")
   entry("Building", "build")
   section("QEX Tutorial", "qex/index.nim"):
-    entry("Getting Started", "getting_started.nim")
+    #entry("Getting Started", "getting_started.nim")
+    entry("Simple example", "simple_example.nim")
     entry("Basic Operations", "basic_ops.nim")
 
 nimibookCli(book)
